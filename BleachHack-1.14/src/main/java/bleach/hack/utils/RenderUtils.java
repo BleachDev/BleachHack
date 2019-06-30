703932ad37d1eb0450d930fe4ddd0d071749c262
Bleach <bruhmoment@number.2>
2019-06-30 19:48:11 +0200

    Clean up RenderUtils

--- BleachHack-1.14/src/main/java/bleach/hack/utils/RenderUtils.java
+++ BleachHack-1.14/src/main/java/bleach/hack/utils/RenderUtils.java
@@ -7,7 +7,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -15,7 +14,6 @@ import net.minecraft.util.math.BlockPos;
 public class RenderUtils {
 
 	private static Minecraft mc = Minecraft.getInstance();
-	private static EntityRendererManager renMang = mc.getRenderManager();
 	
 	public static void drawFilledBox(BlockPos blockPos, float r, float g, float b, float a) {
 		drawFilledBox(new AxisAlignedBB(
@@ -24,13 +22,9 @@ public class RenderUtils {
 	}
 	
 	public static void drawFilledBox(AxisAlignedBB box, float r, float g, float b, float a) {
-		double x = 0, y = 0, z = 0;
-		
-		try {
-			x = box.minX - (double) FieldUtils.readField(renMang, "renderPosX", true);
-			y = box.minY - (double) FieldUtils.readField(renMang, "renderPosY", true);
-	        z = box.minZ - (double) FieldUtils.readField(renMang, "renderPosZ", true);
-		} catch (Exception e) {}
+		double x = box.minX - getRenderPos()[0];
+		double y = box.minY - getRenderPos()[1];
+	    double z = box.minZ - getRenderPos()[2];
         
         gl11Setup();
         
@@ -40,10 +34,7 @@ public class RenderUtils {
         WorldRenderer.addChainedFilledBoxVertices(bufferbuilder,
         		x, y, z, x+box.getXSize(), y+box.getYSize(), z+box.getZSize(), r, g, b, a/2f);
         tessellator.draw();
-        
-        GL11.glLineWidth(2.0F);
-        GL11.glColor4d(0, 1, 0, 0.15F);
-        GL11.glColor4d(0, 0, 1, 0.5F);
+
         WorldRenderer.drawSelectionBoundingBox(new AxisAlignedBB(
         		x, y, z, x+box.getXSize(), y+box.getYSize(), z+box.getZSize()), r, g, b, a);
 
@@ -57,23 +48,25 @@ public class RenderUtils {
 	}
 	
 	public static void drawSelectionBox(AxisAlignedBB box, float r, float g, float b, float a) {
-		double x = 0, y = 0, z = 0;
-		
-		try {
-			x = box.minX - (double) FieldUtils.readField(renMang, "renderPosX", true);
-			y = box.minY - (double) FieldUtils.readField(renMang, "renderPosY", true);
-	        z = box.minZ - (double) FieldUtils.readField(renMang, "renderPosZ", true);
-		} catch (Exception e) {}
+		double x = box.minX - getRenderPos()[0];
+		double y = box.minY - getRenderPos()[1];
+		double z = box.minZ - getRenderPos()[2];
         
         gl11Setup();
-        GL11.glLineWidth(2.0F);
-        GL11.glColor4d(0, 1, 0, 0.15F);
-        GL11.glColor4d(0, 0, 1, 0.5F);
         WorldRenderer.drawSelectionBoundingBox(new AxisAlignedBB(
         		x, y, z, x+box.getXSize(), y+box.getYSize(), z+box.getZSize()), r, g, b, a);
 
         gl11Cleanup();
     }
+	
+	public static double[] getRenderPos() {
+		double x = 0, y = 0, z = 0;
+		try { x = (double) FieldUtils.readField(mc.getRenderManager(), "renderPosX", true);
+			y = (double) FieldUtils.readField(mc.getRenderManager(), "renderPosY", true);
+	        z = (double) FieldUtils.readField(mc.getRenderManager(), "renderPosZ", true);
+		} catch (Exception e) {}
+		return new double[] {x,y,z};
+	}
 	
 	public static void gl11Setup() {
 		GL11.glBlendFunc(770, 771);
@@ -81,6 +74,7 @@ public class RenderUtils {
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glDepthMask(false);
+        GL11.glLineWidth(2.0F);
 	}
 	
 	public static void gl11Cleanup() {
