6898f108b2f0070d57866bc2890b346174ab07f9
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2019-10-27 13:02:12 +0100

    7 commits
    its 7 don't question it

--- BleachHack-Fabric/src/main/java/bleach/hack/event/events/EventKeyPress.java
+++ BleachHack-Fabric/src/main/java/bleach/hack/event/events/EventKeyPress.java
@@ -4,12 +4,18 @@ import bleach.hack.event.Event;
 
 public class EventKeyPress extends Event {
     private int key;
+    private int scanCode;
 
-    public EventKeyPress(int key){
+    public EventKeyPress(int key, int scanCode){
         this.key = key;
+        this.scanCode = scanCode;
     }
 
     public int getKey() {
         return key;
+    }
+    
+    public int getScanCode() {
+    	return scanCode;
     }
 }
--- BleachHack-Fabric/src/main/java/bleach/hack/module/mods/Step.java
+++ BleachHack-Fabric/src/main/java/bleach/hack/module/mods/Step.java
@@ -2,6 +2,7 @@ package bleach.hack.module.mods;
 
 import bleach.hack.event.events.EventTick;
 import bleach.hack.gui.clickgui.SettingMode;
+import bleach.hack.gui.clickgui.SettingToggle;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
 import bleach.hack.utils.WorldUtils;
@@ -15,7 +16,8 @@ public class Step extends Module {
 	
 	public Step() {
 		super("Step", -1, Category.MOVEMENT, "Allows you to Run up blocks like stairs.",
-				new SettingMode("Mode: ", "Simple", "Spider", "Jump"));
+				new SettingMode("Mode: ", "Simple", "Spider", "Jump"),
+				new SettingToggle(false, "Down"));
 	}
 
 	@Override
@@ -26,6 +28,11 @@ public class Step extends Module {
 
 	@Subscribe
 	public void onTick(EventTick event) {
+		if(getSettings().get(1).toToggle().state && !mc.player.onGround && mc.player.fallDistance > 0.1
+				&& !WorldUtils.NONSOLID_BLOCKS.contains(mc.world.getBlockState(mc.player.getBlockPos().add(0, -1, 0)).getBlock())) {
+			mc.player.addVelocity(0, -1, 0);
+		}
+		
 		if(!WorldUtils.NONSOLID_BLOCKS.contains(
 				mc.world.getBlockState(mc.player.getBlockPos().add(0, mc.player.getHeight()+1, 0)).getBlock())) return;
 		
