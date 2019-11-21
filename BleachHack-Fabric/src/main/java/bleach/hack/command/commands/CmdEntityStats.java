356c912267cc546417938465638d3619b15fafa1
d1gress <55198830+d1gress@users.noreply.github.com>
2019-11-21 22:01:30 +0800

    ok

--- BleachHack-Fabric/src/main/java/bleach/hack/command/commands/CmdEntityStats.java
+++ BleachHack-Fabric/src/main/java/bleach/hack/command/commands/CmdEntityStats.java
@@ -45,14 +45,14 @@ public class CmdEntityStats extends Command {
                     mc.player.getVehicle() instanceof LlamaEntity ||
                     mc.player.getVehicle() instanceof MuleEntity) {
                 HorseBaseEntity h = (HorseBaseEntity) mc.player.getVehicle();
-                maxHealth = "" + h.getHealthMaximum();
-                speed = round(1000 * h.getMovementSpeed(), 2) + "% Walk Speed";
-                jumpHeight = "" + round(-0.1817584952 * Math.pow(h.getJumpStrength(), 3) + 3.689713992 * Math.pow(h.getJumpStrength(), 2) + 2.128599134 * h.getJumpStrength() - 0.343930367, 4);
+                maxHealth = h.getHealthMaximum() + " HP";
+                speed = round((100 + 10 * h.getMovementSpeed()) * 4.317, 2) + " m/s";
+                jumpHeight = round(-0.1817584952 * Math.pow(h.getJumpStrength(), 3) + 3.689713992 * Math.pow(h.getJumpStrength(), 2) + 2.128599134 * h.getJumpStrength() - 0.343930367, 4) + " m";
                 BleachLogger.infoMessage("\n§6Entity Stats:\n§cMax Health: §b" + maxHealth + "\n§cSpeed: §b" + speed + "\n§cJump: §b" + jumpHeight);
             }else if (mc.player.getVehicle() instanceof LivingEntity) {
                 LivingEntity l = (LivingEntity) mc.player.getVehicle();
-                maxHealth = "" + l.getHealthMaximum();
-                speed = "" + (int) (1000 * l.getMovementSpeed());
+                maxHealth = l.getHealthMaximum() + " HP";
+                speed = round((100 + 10 * l.getMovementSpeed()) * 4.317, 2) + " m/s";
                 BleachLogger.infoMessage("\n§6Entity Stats:\n§cMax Health: §b" + maxHealth + "\n§cSpeed: §b" + speed);
             }
         }else {
