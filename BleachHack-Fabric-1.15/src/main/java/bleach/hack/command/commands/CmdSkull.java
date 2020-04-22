8767ca7ceae5dd47983012b233854f0c72673bc9
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2020-04-22 13:42:45 +0200

    Add license

--- BleachHack-Fabric-1.15/src/main/java/bleach/hack/command/commands/CmdSkull.java
+++ BleachHack-Fabric-1.15/src/main/java/bleach/hack/command/commands/CmdSkull.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the BleachHack distribution (https://github.com/BleachDrinker420/bleachhack-1.14/).
+ * Copyright (c) 2019 Bleach.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package bleach.hack.command.commands;
 
 import java.util.Base64;
@@ -5,6 +22,7 @@ import java.util.UUID;
 import bleach.hack.command.Command;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.StringNbtReader;
 
 public class CmdSkull extends Command {
@@ -31,9 +49,11 @@ public class CmdSkull extends Command {
 		if(args.length < 2) {
 			item.setTag(StringNbtReader.parse("{SkullOwner:{Name:\"" + args[0] + "\"}}"));
 		}else if(args[0].equalsIgnoreCase("img")) {
-			item.setTag(StringNbtReader.parse("{SkullOwner:{Id:\"" + UUID.randomUUID() + "\",Properties:{textures:[{Value:\""
+			CompoundTag tag = StringNbtReader.parse("{SkullOwner:{Id:\"" + UUID.randomUUID() + "\",Properties:{textures:[{Value:\""
 					+ Base64.getEncoder().encodeToString(("{\"textures\":{\"SKIN\":{\"url\":\"" + args[1] + "\"}}}").getBytes())
-					+ "\"}]}}}"));
+					+ "\"}]}}}");
+			item.setTag(tag);
+			System.out.println(tag);
 		}
 		
 		mc.player.inventory.addPickBlock(item);
