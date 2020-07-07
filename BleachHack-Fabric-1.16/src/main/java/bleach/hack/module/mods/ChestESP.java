c7a87281f493588c665b33278165b56cf1f39efb
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2020-07-07 10:23:43 +0200

    Clean ChestESP up and make it better fit to chests

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/ChestESP.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/ChestESP.java
@@ -23,17 +23,19 @@ import bleach.hack.module.Category;
 import bleach.hack.module.Module;
 import bleach.hack.utils.RenderUtils;
 import com.google.common.eventbus.Subscribe;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
+import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.block.entity.BarrelBlockEntity;
-import net.minecraft.block.entity.BlastFurnaceBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BrewingStandBlockEntity;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.block.entity.DispenserBlockEntity;
 import net.minecraft.block.entity.EnderChestBlockEntity;
-import net.minecraft.block.entity.FurnaceBlockEntity;
 import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.block.entity.ShulkerBoxBlockEntity;
-import net.minecraft.block.entity.SmokerBlockEntity;
+import net.minecraft.block.enums.ChestType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.decoration.ItemFrameEntity;
@@ -41,6 +43,9 @@ import net.minecraft.entity.vehicle.ChestMinecartEntity;
 import net.minecraft.entity.vehicle.FurnaceMinecartEntity;
 import net.minecraft.entity.vehicle.HopperMinecartEntity;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
 
 public class ChestESP extends Module {
 	
@@ -63,33 +68,33 @@ public class ChestESP extends Module {
 	@Subscribe
 	public void onRender(Event3DRender event) {
 		for (BlockEntity e: mc.world.blockEntities) {
-			if ((e instanceof ChestBlockEntity || e instanceof BarrelBlockEntity)
-					&& getSettings().get(0).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getPos(), 1.9F, 1.5F, 0.3F, 0.7F);}
-			if (e instanceof EnderChestBlockEntity && getSettings().get(1).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getPos(), 1F, 0.05F, 1F, 0.7F);}
-			if ((e instanceof FurnaceBlockEntity || e instanceof SmokerBlockEntity ||
-					e instanceof BlastFurnaceBlockEntity) && getSettings().get(2).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getPos(), 0.5F, 0.5F, 0.5F, 0.7F);}
-			if (e instanceof DispenserBlockEntity && getSettings().get(3).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getPos(), 0.55F, 0.55F, 0.7F, 0.7F);}
-			if (e instanceof HopperBlockEntity && getSettings().get(4).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getPos(), 0.45F, 0.45F, 0.6F, 0.7F);}
-			if (e instanceof ShulkerBoxBlockEntity && getSettings().get(5).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getPos(), 0.5F, 0.2F, 1F, 0.7F);}
-			if (e instanceof BrewingStandBlockEntity && getSettings().get(6).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getPos(), 0.5F, 0.4F, 0.2F, 0.7F);}
+			if ((e instanceof ChestBlockEntity || e instanceof BarrelBlockEntity) && getSettings().get(0).toToggle().state) {
+				drawChest(e.getPos()); //RenderUtils.drawFilledBox(e.getPos(), 1.9F, 1.5F, 0.3F, 0.7F);
+			} else if (e instanceof EnderChestBlockEntity && getSettings().get(1).toToggle().state) {
+				RenderUtils.drawFilledBox(new Box(
+						e.getPos().getX() + 0.06, e.getPos().getY(), e.getPos().getZ() + 0.06,
+						e.getPos().getX() + 0.94, e.getPos().getY() + 0.875, e.getPos().getZ() + 0.94), 1F, 0.05F, 1F, 0.7F);
+			} else if (e instanceof AbstractFurnaceBlockEntity && getSettings().get(2).toToggle().state) {
+				RenderUtils.drawFilledBox(e.getPos(), 0.5F, 0.5F, 0.5F, 0.7F);
+			} else if (e instanceof DispenserBlockEntity && getSettings().get(3).toToggle().state) {
+				RenderUtils.drawFilledBox(e.getPos(), 0.55F, 0.55F, 0.7F, 0.7F);
+			} else if (e instanceof HopperBlockEntity && getSettings().get(4).toToggle().state) {
+				RenderUtils.drawFilledBox(e.getPos(), 0.45F, 0.45F, 0.6F, 0.7F);
+			} else if (e instanceof ShulkerBoxBlockEntity && getSettings().get(5).toToggle().state) {
+				RenderUtils.drawFilledBox(e.getPos(), 0.5F, 0.2F, 1F, 0.7F);
+			} else if (e instanceof BrewingStandBlockEntity && getSettings().get(6).toToggle().state) {
+				RenderUtils.drawFilledBox(e.getPos(), 0.5F, 0.4F, 0.2F, 0.7F);
+			}
 		}
 		
 		for (Entity e: mc.world.getEntities()) {
 			if (e instanceof ChestMinecartEntity && getSettings().get(7).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getBoundingBox(), 1.9F, 1.5F, 0.3F, 0.7F);}
-			if (e instanceof FurnaceMinecartEntity && getSettings().get(8).toToggle().state) {
+				RenderUtils.drawFilledBox(e.getBoundingBox(), 1.9F, 1.5F, 0.3F, 0.7F);
+			} else if (e instanceof FurnaceMinecartEntity && getSettings().get(8).toToggle().state) {
 				RenderUtils.drawFilledBox(e.getBoundingBox(), 0.5F, 0.5F, 0.5F, 0.7F);}
-			if (e instanceof HopperMinecartEntity && getSettings().get(9).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getBoundingBox(), 0.45F, 0.45F, 0.6F, 0.7F);}
-			
-			if (e instanceof ItemFrameEntity && getSettings().get(10).toToggle().state) {
+			else if (e instanceof HopperMinecartEntity && getSettings().get(9).toToggle().state) {
+				RenderUtils.drawFilledBox(e.getBoundingBox(), 0.45F, 0.45F, 0.6F, 0.7F);
+			} else if (e instanceof ItemFrameEntity && getSettings().get(10).toToggle().state) {
 				if (((ItemFrameEntity) e).getHeldItemStack().getItem() == Items.AIR) {
 					RenderUtils.drawFilledBox(e.getBoundingBox(), 0.45F, 0.1F, 0.1F, 0.7F);
 				} else if (((ItemFrameEntity) e).getHeldItemStack().getItem() == Items.FILLED_MAP) {
@@ -100,8 +105,43 @@ public class ChestESP extends Module {
 			}
 			
 			if (e instanceof ArmorStandEntity && getSettings().get(11).toToggle().state) {
-				RenderUtils.drawFilledBox(e.getBoundingBox(), 0.5F, 0.4F, 0.1F, 0.7F);}
+				RenderUtils.drawFilledBox(e.getBoundingBox(), 0.5F, 0.4F, 0.1F, 0.7F);
+			}
 		}
+	}
+	
+	private void drawChest(BlockPos pos) {
+		BlockState state = mc.world.getBlockState(pos);
+		
+		if (!(state.getBlock() instanceof ChestBlock)) {
+			RenderUtils.drawFilledBox(pos, 1.9F, 1.5F, 0.3F, 0.7F);
+			return;
+		}
+		
+		if (state.get(ChestBlock.CHEST_TYPE) == ChestType.SINGLE) {
+			RenderUtils.drawFilledBox(new Box(
+					pos.getX() + 0.06, pos.getY(), pos.getZ() + 0.06,
+					pos.getX() + 0.94, pos.getY() + 0.875, pos.getZ() + 0.94), 1.9F, 1.5F, 0.3F, 0.7F);
+			return;
+		}
+		
+		boolean north = false, east = false, south = false, west = false;
+		
+		Direction dir = ChestBlock.getFacing(state);
+		if (dir == Direction.NORTH) {
+			north = true;
+		} else if (dir == Direction.EAST) {
+			east = true;
+		} else if (dir == Direction.SOUTH) {
+			south = true;
+		} else if (dir == Direction.WEST) {
+			west = true;
+		}
+		
+		RenderUtils.drawFilledBox(new Box(
+				west ? pos.getX() : pos.getX() + 0.06, pos.getY(), north ? pos.getZ() : pos.getZ() + 0.06,
+				east ? pos.getX() + 1 : pos.getX() + 0.94, pos.getY() + 0.875, south ? pos.getZ() + 1 : pos.getZ() + 0.94),
+				1.9F, 1.5F, 0.3F, 0.7F);
 	}
 
 }
