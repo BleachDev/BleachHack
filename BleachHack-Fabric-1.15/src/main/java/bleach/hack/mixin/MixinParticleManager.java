7c6dee28a4e0d4b1110e22d74e2c84b9aee2f49e
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2020-07-29 09:31:36 +0200

    Attempt to save the particle event

--- BleachHack-Fabric-1.15/src/main/java/bleach/hack/mixin/MixinParticleManager.java
+++ BleachHack-Fabric-1.15/src/main/java/bleach/hack/mixin/MixinParticleManager.java
@@ -5,21 +5,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import bleach.hack.BleachHack;
+import bleach.hack.event.events.EventParticle;
 import bleach.hack.module.ModuleManager;
 import bleach.hack.module.mods.Nuker;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.util.math.BlockPos;
 
 @Mixin(ParticleManager.class)
 public class MixinParticleManager {
-
+	
 	@Inject(at = @At("HEAD"), method = "addParticle(Lnet/minecraft/client/particle/Particle;)V", cancellable = true)
 	public void addParticle(Particle particle_1, CallbackInfo ci) {
 		// pls send help
-		/*EventParticle event = new EventParticle(particle_1);
+		EventParticle event = new EventParticle(particle_1);
 		BleachHack.eventBus.post(event);
-		if (event.isCancelled()) ci.cancel();*/
-		
+		if (event.isCancelled()) ci.cancel();
+	}
+	
+	/** Mojang is retarded and makes this and ONLY this thread-unsafe **/
+	@Inject(at = @At("HEAD"), method = "addBlockBreakParticles", cancellable = true)
+	public void addBlockBreakParticles(BlockPos blockPos_1, BlockState blockState_1, CallbackInfo ci) {
 		if (ModuleManager.getModule(Nuker.class).isToggled() && ModuleManager.getModule(Nuker.class).getSetting(6).asToggle().state) {
 			ci.cancel();
 		}
