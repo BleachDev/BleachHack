78cc1ca6153036dc3f524e2ab4223c7995419296
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2020-08-07 16:20:48 +0200

    Add Smooth setting to zoom

--- BleachHack-Fabric-1.15/src/main/java/bleach/hack/module/mods/Zoom.java
+++ BleachHack-Fabric-1.15/src/main/java/bleach/hack/module/mods/Zoom.java
@@ -18,29 +18,69 @@
  
  package bleach.hack.module.mods;
 
+
+import com.google.common.eventbus.Subscribe;
+
+import bleach.hack.event.events.EventTick;
 import bleach.hack.gui.clickgui.SettingSlider;
+import bleach.hack.gui.clickgui.SettingToggle;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
+import bleach.hack.utils.BleachQueue;
 
 public class Zoom extends Module {
-
-	public Zoom() {
-		super("Zoom", KEY_UNBOUND, Category.RENDER, "zooms",
-				new SettingSlider("Scale: ", 1, 10, 3, 1));
-	}
-
+	
 	public double prevFov;
 	public double prevSens;
-	public void onEnable() {
-		prevFov = mc.options.fov;
-		mc.options.fov = prevFov / getSetting(0).asSlider().getValue();
-		prevSens = mc.options.mouseSensitivity;
-		mc.options.mouseSensitivity = prevSens / getSetting(0).asSlider().getValue();
-		super.onEnable();
+
+	public Zoom() {
+		super("Zoom", KEY_UNBOUND, Category.RENDER, "ok zoomer",
+				new SettingSlider("Scale: ", 1, 10, 3, 2).withDesc("How much to zoom"),
+				new SettingToggle("Smooth", false).withDesc("Smooths the zoom when enabling and disabling"));
 	}
+	
+	public void onEnable() {
+		super.onEnable();
+		BleachQueue.cancelQueue("zoom");
+		
+		prevFov = mc.options.fov;
+		prevSens = mc.options.mouseSensitivity;
+		
+		if (getSetting(0).asSlider().getValue() < 1 || !getSetting(1).asToggle().state) {
+			mc.options.fov = prevFov / getSetting(0).asSlider().getValue();
+			mc.options.mouseSensitivity = prevSens / getSetting(0).asSlider().getValue();
+		}
+	}
+	
 	public void onDisable() {
-		mc.options.fov = prevFov;
-		mc.options.mouseSensitivity = prevSens;
+		if (getSetting(0).asSlider().getValue() < 1 || !getSetting(1).asToggle().state) {
+			mc.options.fov = prevFov;
+			mc.options.mouseSensitivity = prevSens;
+		} else {
+			for (double f = mc.options.fov; f < prevFov; f *= 1.4) {
+				BleachQueue.add("zoom", () -> {
+					mc.options.fov = Math.min(prevFov, mc.options.fov * 1.4);
+					mc.options.mouseSensitivity = Math.min(prevSens, mc.options.mouseSensitivity * 1.4);
+				});
+			}
+		}
+		
 		super.onDisable();
+	}
+	
+	@Subscribe
+	public void onTick(EventTick event) {
+		if (getSetting(0).asSlider().getValue() >= 1 && mc.options.fov > prevFov / getSetting(0).asSlider().getValue()) {
+			mc.options.fov /= 1.4;
+			mc.options.mouseSensitivity /= 1.4;
+			
+			if (mc.options.fov <= prevFov / getSetting(0).asSlider().getValue()) {
+				mc.options.fov = prevFov / getSetting(0).asSlider().getValue();
+				mc.options.mouseSensitivity = prevSens / getSetting(0).asSlider().getValue();
+			}
+		} else {
+			mc.options.fov = prevFov / getSetting(0).asSlider().getValue();
+			mc.options.mouseSensitivity = prevSens / getSetting(0).asSlider().getValue();
+		}
 	}
 }
