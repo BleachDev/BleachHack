6abee35c162fa05d4cda746d83dcd5063a75f320
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2020-08-14 18:41:33 +0200

    Actually finish autoarmor

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/AutoArmor.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/AutoArmor.java
@@ -20,6 +20,7 @@ import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ElytraItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.ToolItem;
 import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.screen.slot.SlotActionType;
 
@@ -30,7 +31,6 @@ public class AutoArmor extends Module {
 	public AutoArmor() {
 		super("AutoArmor", KEY_UNBOUND, Category.PLAYER, "Automatically equips armor",
 				new SettingToggle("Anti Break", false).withDesc("Unequips your armor when its about to break"),
-				new SettingToggle("Legit Equip", false).withDesc("Freezes you when it would be impossible to equip the armor without going in your inventory"),
 				new SettingToggle("Prefer Elytra", false).withDesc("Equips elytras instead of chestplates when possible"),
 				new SettingToggle("Delay", true).withDesc("Adds a delay between equipping armor pieces to make it bypass more anticheats").withChildren(
 						new SettingSlider("Delay", 0, 20, 1, 0).withDesc("How many ticks between putting on armor pieces")));
@@ -46,7 +46,7 @@ public class AutoArmor extends Module {
 			return;
 		}
 
-		tickDelay = (getSetting(3).asToggle().state ? (int) getSetting(3).asToggle().getChild(0).asSlider().getValue() : 0);
+		tickDelay = (getSetting(2).asToggle().state ? (int) getSetting(2).asToggle().getChild(0).asSlider().getValue() : 0);
 
 		/* [Slot type, [Armor slot, Armor prot, New armor slot, New armor prot]] */
 		HashMap<EquipmentSlot, int[]> armorMap = new HashMap<>(4);
@@ -54,12 +54,49 @@ public class AutoArmor extends Module {
 		armorMap.put(EquipmentSlot.LEGS, new int[] { 37, getProtection(mc.player.inventory.getStack(37)), -1, -1 });
 		armorMap.put(EquipmentSlot.CHEST, new int[] { 38, getProtection(mc.player.inventory.getStack(38)), -1, -1 });
 		armorMap.put(EquipmentSlot.HEAD, new int[] { 39, getProtection(mc.player.inventory.getStack(39)), -1, -1 });
+		
+		/* Anti Break */
+		if (getSetting(0).asToggle().state) {
+			for (Entry<EquipmentSlot, int[]> e: armorMap.entrySet()) {
+				ItemStack is = mc.player.inventory.getStack(e.getValue()[0]);
+				int armorSlot = (e.getValue()[0] - 34) + (39 - e.getValue()[0]) * 2;
+				
+				if (is.isDamageable() && is.getMaxDamage() - is.getDamage() < 7) {
+					/* Look for an empty slot to quick move to */
+					int forceMoveSlot = -1;
+					for (int s = 0; s < 36; s++) {
+						if (mc.player.inventory.getStack(s).isEmpty()) {
+							mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, armorSlot, 1, SlotActionType.QUICK_MOVE, mc.player);
+							return;
+						} else if (!(mc.player.inventory.getStack(s).getItem() instanceof ToolItem)
+								&& !(mc.player.inventory.getStack(s).getItem() instanceof ArmorItem)
+								&& !(mc.player.inventory.getStack(s).getItem() instanceof ElytraItem)
+								&& mc.player.inventory.getStack(s).getItem() != Items.TOTEM_OF_UNDYING && forceMoveSlot == -1) {
+							forceMoveSlot = s;
+						}
+					}
+					
+					/* Bruh no empty spots, then force move to a non-totem/tool/armor item */
+					if (forceMoveSlot != -1) {
+						//System.out.println(forceMoveSlot);
+						mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId,
+								forceMoveSlot < 9 ? 36 + forceMoveSlot : forceMoveSlot, 1, SlotActionType.THROW, mc.player);
+						mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, armorSlot, 1, SlotActionType.QUICK_MOVE, mc.player);
+						return;
+					}
+					
+					/* No spots to move to, yeet the armor to not cause any bruh moments */
+					mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, armorSlot, 1, SlotActionType.THROW, mc.player);
+					return;
+				}
+			}
+		}
 
 		for (int s = 0; s < 36; s++) {
 			int prot = getProtection(mc.player.inventory.getStack(s));
 
 			if (prot > 0) {
-				EquipmentSlot slot = (mc.player.inventory.getStack(s).getItem() == Items.ELYTRA
+				EquipmentSlot slot = (mc.player.inventory.getStack(s).getItem() instanceof ElytraItem
 						? EquipmentSlot.CHEST : ((ArmorItem) mc.player.inventory.getStack(s).getItem()).getSlotType());
 
 				for (Entry<EquipmentSlot, int[]> e: armorMap.entrySet()) {
@@ -103,14 +140,16 @@ public class AutoArmor extends Module {
 		if (is.getItem() instanceof ArmorItem || is.getItem() == Items.ELYTRA) {
 			int prot = 0;
 			
-			if (is.getItem() == Items.ELYTRA) {
+			if (is.getItem() instanceof ElytraItem) {
 				if (!ElytraItem.isUsable(is))
 					return 0;
 				
-				if (getSetting(2).asToggle().state)
+				if (getSetting(1).asToggle().state)
 					prot = 32767;
 				else
 					prot = 1;
+			} else if (is.getMaxDamage() - is.getDamage() < 7 && getSetting(0).asToggle().state) {
+				return 0;
 			}
 			
 			if (is.hasEnchantments()) {
