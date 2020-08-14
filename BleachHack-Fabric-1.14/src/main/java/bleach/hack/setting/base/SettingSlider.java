9644bf9d60d8d76ba3f9a81e2139f71391c40899
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2020-08-14 09:28:50 +0200

    Rename "round" in SettingSlider to "decimals" to make it more clear

--- BleachHack-Fabric-1.14/src/main/java/bleach/hack/setting/base/SettingSlider.java
+++ BleachHack-Fabric-1.14/src/main/java/bleach/hack/setting/base/SettingSlider.java
@@ -32,23 +32,23 @@ public class SettingSlider extends SettingBase {
 	public double min;
 	public double max;
 	private double value;
-	public int round;
+	public int decimals;
 	public String text;
 
 	protected double defaultValue;
 
-	public SettingSlider(String text, double min, double max, double value, int round) {
+	public SettingSlider(String text, double min, double max, double value, int decimals) {
 		this.min = min;
 		this.max = max;
 		this.value = value;
-		this.round = round;
+		this.decimals = decimals;
 		this.text = text;
 
 		defaultValue = value;
 	}
 
 	public double getValue() {
-		return round(value, round);
+		return round(value, decimals);
 	}
 
 	public void setValue(double value) {
@@ -69,13 +69,13 @@ public class SettingSlider extends SettingBase {
 		int pixels = (int) Math.round(MathHelper.clamp((len - 2) * ((getValue() - min) / (max - min)), 0, len - 2));
 		window.fillGradient(x + 1, y, x + pixels, y + 12, 0xf03080a0, 0xf02070b0);
 
-		MinecraftClient.getInstance().textRenderer.drawWithShadow(text + ": " + (round == 0 && getValue() > 100 ? Integer.toString((int) getValue()) : getValue()),
+		MinecraftClient.getInstance().textRenderer.drawWithShadow(text + ": " + (decimals == 0 && getValue() > 100 ? Integer.toString((int) getValue()) : getValue()),
 				x + 2, y + 2, window.mouseOver(x, y, x + len, y + 12) ? 0xcfc3cf : 0xcfe0cf);
 
 		if (window.mouseOver(x + 1, y, x + len - 2, y + 12) && window.lmHeld) {
 			int percent = ((window.mouseX - x) * 100) / (len - 2);
 
-			setValue(round(percent * ((max - min) / 100) + min, round));
+			setValue(round(percent * ((max - min) / 100) + min, decimals));
 		}
 	}
 
@@ -101,7 +101,7 @@ public class SettingSlider extends SettingBase {
 	@Override
 	public boolean isDefault() {
 		BigDecimal bd = new BigDecimal(defaultValue);
-		bd = bd.setScale(round, RoundingMode.HALF_UP);
+		bd = bd.setScale(decimals, RoundingMode.HALF_UP);
 
 		return bd.doubleValue() == getValue();
 	}
