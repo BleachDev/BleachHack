59574008d1d683513d0463c1037bde3b8f9dd335
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2020-08-15 20:33:47 +0200

    Fix xray on 1.16????

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/mixin/MixinBlockModelRenderer.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/mixin/MixinBlockModelRenderer.java
@@ -17,19 +17,16 @@
  */
 package bleach.hack.mixin;
 
-import java.util.Random;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import bleach.hack.module.ModuleManager;
 import bleach.hack.module.mods.Xray;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.block.BlockModelRenderer;
-import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
@@ -40,13 +37,15 @@ import net.minecraft.world.BlockRenderView;
  */
 @Mixin(BlockModelRenderer.class)
 public class MixinBlockModelRenderer {
-	@Inject(method = "render", at = @At("HEAD"), cancellable = true)
-	private void render(BlockRenderView blockRenderView_1, BakedModel bakedModel_1, BlockState blockState_1, BlockPos blockPos_1, MatrixStack matrixStack_1,
-			VertexConsumer vertexConsumer_1, boolean boolean_1, Random random_1, long long_1, int int_1, CallbackInfoReturnable<Boolean> ci) {
+	
+	// don't use render because sodium small pp
+	@Inject(method = "renderQuad", at = @At("HEAD"), cancellable = true)
+	private void renderQuad(BlockRenderView world, BlockState state, BlockPos pos, VertexConsumer vertexConsumer, MatrixStack.Entry matrixEntry, BakedQuad quad,
+			float brightness0, float brightness1, float brightness2, float brightness3, int light0, int light1, int light2, int light3, int overlay, CallbackInfo ci) {
 		try {
 			Xray xray = (Xray) ModuleManager.getModule(Xray.class);
-			if (!xray.isVisible(blockState_1.getBlock())) {
-				ci.setReturnValue(false);
+			if (!xray.isVisible(state.getBlock())) {
+				//ci.setReturnValue(false);
 				ci.cancel();
 			}
 		} catch (Exception ignored) {
