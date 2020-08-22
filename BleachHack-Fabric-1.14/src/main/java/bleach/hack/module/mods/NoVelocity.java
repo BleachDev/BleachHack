97fa3edf2d348fc0a8f49d3c5511897154d16e60
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2020-08-22 16:12:47 +0200

    Improve NoVelocity
    - Add Knockback and explosion toggled with velocity amount
    - Add Pushing setting with amount
    - Add Fluid setting

--- BleachHack-Fabric-1.14/src/main/java/bleach/hack/module/mods/NoVelocity.java
+++ BleachHack-Fabric-1.14/src/main/java/bleach/hack/module/mods/NoVelocity.java
@@ -20,8 +20,11 @@ package bleach.hack.module.mods;
 import com.google.common.eventbus.Subscribe;
 
 import bleach.hack.event.events.EventReadPacket;
+import bleach.hack.event.events.EventTick;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
+import bleach.hack.setting.base.SettingSlider;
+import bleach.hack.setting.base.SettingToggle;
 import bleach.hack.utils.FabricReflect;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
@@ -29,29 +32,60 @@ import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
 /**
  * @author sl First Module utilizing EventBus!
  */
-
 public class NoVelocity extends Module {
+	
 	public NoVelocity() {
-		super("NoVelocity", KEY_UNBOUND, Category.PLAYER, "If you take some damage, you don't move. Maybe.");
+		super("NoVelocity", KEY_UNBOUND, Category.PLAYER, "If you take some damage, you don't move.",
+				new SettingToggle("Knockback", true).withDesc("Reduces knockback from other entites").withChildren(
+						new SettingSlider("VelXZ", 0, 100, 0, 1).withDesc("How much horizontal velocity"),
+						new SettingSlider("VelY", 0, 100, 0, 1).withDesc("How much vertical velocity")),
+				new SettingToggle("Explosions", true).withDesc("Reduces explosion velocity").withChildren(
+						new SettingSlider("VelXZ", 0, 100, 0, 1).withDesc("How much horizontal velocity"),
+						new SettingSlider("VelY", 0, 100, 0, 1).withDesc("How much vertical velocity")),
+				new SettingToggle("Pushing", true).withDesc("Reduces how much you get pushed by entites").withChildren(
+						new SettingSlider("Amount", 0, 100, 0, 1).withDesc("How much to reduce pushing")),
+				new SettingToggle("Fluids", true).withDesc("Reduces how much you get pushed from fluids"));
+	}
+	
+	public void onDisable() {
+		mc.player.pushSpeedReduction = 0f;
+		
+		super.onDisable();
+	}
+	
+	@Subscribe
+	public void onTick(EventTick event) {
+		if (getSetting(2).asToggle().state) {
+			mc.player.pushSpeedReduction = (float) (1 - getSetting(2).asToggle().getChild(0).asSlider().getValue() / 100);
+		}
 	}
 
-	// The name of the method doesn't matter nor does it need to be consistent
-	// between modules, what matters is the argument.
 	@Subscribe
 	public void readPacket(EventReadPacket event) {
 		if (mc.player == null)
 			return;
-		if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket) {
+		
+		if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket && getSetting(0).asToggle().state) {
 			EntityVelocityUpdateS2CPacket packet = (EntityVelocityUpdateS2CPacket) event.getPacket();
 			if (packet.getId() == mc.player.getEntityId()) {
-				FabricReflect.writeField(packet, 0, "field_12563", "velocityX");
-				FabricReflect.writeField(packet, 0, "field_12562", "velocityY");
-				FabricReflect.writeField(packet, 0, "field_12561", "velocityZ");
+				double velXZ = getSetting(0).asToggle().getChild(0).asSlider().getValue() / 100;
+				double velY = getSetting(0).asToggle().getChild(1).asSlider().getValue() / 100;
+				
+				FabricReflect.writeField(packet, packet.getVelocityX() * velXZ, "field_12563", "velocityX");
+				FabricReflect.writeField(packet, packet.getVelocityY() * velY, "field_12562", "velocityY");
+				FabricReflect.writeField(packet, packet.getVelocityZ() * velXZ, "field_12561", "velocityZ");
 			}
-		} else if (event.getPacket() instanceof ExplosionS2CPacket) {
-			FabricReflect.writeField(event.getPacket(), 0, "field_12176", "playerVelocityX");
-			FabricReflect.writeField(event.getPacket(), 0, "field_12182", "playerVelocityY");
-			FabricReflect.writeField(event.getPacket(), 0, "field_12183", "playerVelocityZ");
+		} else if (event.getPacket() instanceof ExplosionS2CPacket && getSetting(1).asToggle().state) {
+			EntityVelocityUpdateS2CPacket packet = (EntityVelocityUpdateS2CPacket) event.getPacket();
+			
+			double velXZ = getSetting(1).asToggle().getChild(0).asSlider().getValue() / 100;
+			double velY = getSetting(1).asToggle().getChild(1).asSlider().getValue() / 100;
+			
+			FabricReflect.writeField(event.getPacket(), packet.getVelocityX() * velXZ, "field_12176", "playerVelocityX");
+			FabricReflect.writeField(event.getPacket(), packet.getVelocityY() * velY, "field_12182", "playerVelocityY");
+			FabricReflect.writeField(event.getPacket(), packet.getVelocityZ() * velXZ, "field_12183", "playerVelocityZ");
 		}
 	}
+	
+	// Fluid handling in MixinFlowableFluid.getVelocity_hasNext()
 }
