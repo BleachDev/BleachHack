61b890064d681d1d130b4c404683bbf7e968af15
Ian <58058658+22s@users.noreply.github.com>
2020-10-21 12:31:14 -0700

    Update AutoTrap.java

--- src/main/java/bleach/hack/module/mods/AutoTrap.java
+++ src/main/java/bleach/hack/module/mods/AutoTrap.java
@@ -20,7 +20,7 @@ import net.minecraft.util.math.Direction;
 public class AutoTrap extends Module {
 
     public AutoTrap() {
-        super("AutoTrap", KEY_UNBOUND, Category.CHAT, "autotrap fortnite. stolen from meteor client bc i cba placing block or doing maffs",
+        super("AutoTrap", KEY_UNBOUND, Category.CHAT, "autotraps other players near you, does not trap friends!",
                 new SettingMode("Mode", "Top", "Full", "Bed Aura")
             );
     }
