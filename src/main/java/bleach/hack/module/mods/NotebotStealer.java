a9d94f36f049dd0c942d884c42c11a5542b3ea7a
DarkReaper231 <38611351+DarkReaper231@users.noreply.github.com>
2021-01-01 15:37:13 +0100

    Update NotebotStealer.java

--- src/main/java/bleach/hack/module/mods/NotebotStealer.java
+++ src/main/java/bleach/hack/module/mods/NotebotStealer.java
@@ -38,105 +38,105 @@ import net.minecraft.sound.SoundCategory;
 
 public class NotebotStealer extends Module {
 
-	private List<List<Integer>> notes = new ArrayList<>();
-	private Multimap<SoundCategory, SoundInstance> prevSoundMap = HashMultimap.create();
-	private int ticks = 0;
+    private List<List<Integer>> notes = new ArrayList<>();
+    private Multimap<SoundCategory, SoundInstance> prevSoundMap = HashMultimap.create();
+    private int ticks = 0;
 
-	public NotebotStealer() {
-		super("NotebotStealer", KEY_UNBOUND, Category.MISC, "Steals noteblock songs");
-	}
+    public NotebotStealer() {
+        super("NotebotStealer", KEY_UNBOUND, Category.MISC, "Steals noteblock songs");
+    }
 
-	@Override
-	public void onEnable() {
-		super.onEnable();
-		notes.clear();
-		prevSoundMap.clear();
-		ticks = 0;
-	}
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        notes.clear();
+        prevSoundMap.clear();
+        ticks = 0;
+    }
 
-	@Override
-	public void onDisable() {
-		super.onDisable();
-		int i = 0;
-		String s = "";
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        int i = 0;
+        String s = "";
 
-		// TODO: Figure out how to use BleachFileMang instead
-		File theDir = new File("./bleach/notebot");
-		if (!theDir.exists()){
-			theDir.mkdirs();
-		}
+        // TODO: Figure out how to use BleachFileMang instead
+        File theDir = new File("./bleach/notebot");
+        if (!theDir.exists()) {
+            theDir.mkdirs();
+        }
 
-		while (BleachFileMang.fileExists("notebot", "notebot" + i + ".txt"))
-			i++;
-		for (List<Integer> i1 : notes)
-			s += i1.get(0) + ":" + i1.get(1) + ":" + i1.get(2) + "\n";
-		BleachFileMang.createFile("notebot", "notebot" + i + ".txt");
-		BleachFileMang.appendFile(s, "notebot", "notebot" + i + ".txt");
-		BleachLogger.infoMessage("Saved Song As: notebot" + i + ".txt [" + notes.size() + " Notes]");
-	}
+        while (BleachFileMang.fileExists("notebot", "notebot" + i + ".txt"))
+            i++;
+        for (List<Integer> i1 : notes)
+            s += i1.get(0) + ":" + i1.get(1) + ":" + i1.get(2) + "\n";
+        BleachFileMang.createFile("notebot", "notebot" + i + ".txt");
+        BleachFileMang.appendFile(s, "notebot", "notebot" + i + ".txt");
+        BleachLogger.infoMessage("Saved Song As: notebot" + i + ".txt [" + notes.size() + " Notes]");
+    }
 
-	@SuppressWarnings("unchecked")
-	@Subscribe
-	public void onTick(EventTick event) {
-		Multimap<SoundCategory, SoundInstance> soundMap = (Multimap<SoundCategory, SoundInstance>) FabricReflect.getFieldValue(
-				FabricReflect.getFieldValue(mc.getSoundManager(), "field_5590", "soundSystem"), "field_18951", "sounds");
+    @SuppressWarnings("unchecked")
+    @Subscribe
+    public void onTick(EventTick event) {
+        Multimap<SoundCategory, SoundInstance> soundMap = (Multimap<SoundCategory, SoundInstance>) FabricReflect.getFieldValue(
+                FabricReflect.getFieldValue(mc.getSoundManager(), "field_5590", "soundSystem"), "field_18951", "sounds");
 
-		for (Entry<SoundCategory, SoundInstance> e : HashMultimap.create(soundMap).entries()) {
-			if (prevSoundMap.containsEntry(e.getKey(), e.getValue()))
-				soundMap.remove(e.getKey(), e.getValue());
-		}
+        for (Entry<SoundCategory, SoundInstance> e : HashMultimap.create(soundMap).entries()) {
+            if (prevSoundMap.containsEntry(e.getKey(), e.getValue()))
+                soundMap.remove(e.getKey(), e.getValue());
+        }
 
-		for (Entry<SoundCategory, SoundInstance> e : soundMap.entries()) {
-			if (e.getValue().getId().getPath().contains("note_block")) {
-				int type = 0;
-				int note = 0;
+        for (Entry<SoundCategory, SoundInstance> e : soundMap.entries()) {
+            if (e.getValue().getId().getPath().contains("note_block")) {
+                int type = 0;
+                int note = 0;
 
-				for (int n = 0; n < 25; n++) {
-					if ((float) Math.pow(2.0D, (n - 12) / 12.0D) - 0.01 < e.getValue().getPitch() &&
-							(float) Math.pow(2.0D, (n - 12) / 12.0D) + 0.01 > e.getValue().getPitch()) {
-						note = n;
-						break;
-					}
-				}
+                for (int n = 0; n < 25; n++) {
+                    if ((float) Math.pow(2.0D, (n - 12) / 12.0D) - 0.01 < e.getValue().getPitch() &&
+                            (float) Math.pow(2.0D, (n - 12) / 12.0D) + 0.01 > e.getValue().getPitch()) {
+                        note = n;
+                        break;
+                    }
+                }
 
-				if (e.getValue().getId().getPath().contains("basedrum"))
-					type = 1;
-				else if (e.getValue().getId().getPath().contains("snare"))
-					type = 2;
-				else if (e.getValue().getId().getPath().contains("hat"))
-					type = 3;
-				else if (e.getValue().getId().getPath().contains("bass"))
-					type = 4;
-				else if (e.getValue().getId().getPath().contains("flute"))
-					type = 5;
-				else if (e.getValue().getId().getPath().contains("bell"))
-					type = 6;
-				else if (e.getValue().getId().getPath().contains("guitar"))
-					type = 7;
-				else if (e.getValue().getId().getPath().contains("chime"))
-					type = 8;
-				else if (e.getValue().getId().getPath().contains("xylophone"))
-					type = 9;
-				else if (e.getValue().getId().getPath().contains("iron_xylophone"))
-					type = 10;
-				else if (e.getValue().getId().getPath().contains("cow_bell"))
-					type = 11;
-				else if (e.getValue().getId().getPath().contains("didgeridoo"))
-					type = 12;
-				else if (e.getValue().getId().getPath().contains("bit"))
-					type = 13;
-				else if (e.getValue().getId().getPath().contains("banjo"))
-					type = 14;
-				else if (e.getValue().getId().getPath().contains("pling"))
-					type = 15;
-				notes.add(Arrays.asList(ticks + 0, note, type));
-			}
-		}
+                if (e.getValue().getId().getPath().contains("basedrum"))
+                    type = 1;
+                else if (e.getValue().getId().getPath().contains("snare"))
+                    type = 2;
+                else if (e.getValue().getId().getPath().contains("hat"))
+                    type = 3;
+                else if (e.getValue().getId().getPath().contains("bass"))
+                    type = 4;
+                else if (e.getValue().getId().getPath().contains("flute"))
+                    type = 5;
+                else if (e.getValue().getId().getPath().contains("bell"))
+                    type = 6;
+                else if (e.getValue().getId().getPath().contains("guitar"))
+                    type = 7;
+                else if (e.getValue().getId().getPath().contains("chime"))
+                    type = 8;
+                else if (e.getValue().getId().getPath().contains("xylophone"))
+                    type = 9;
+                else if (e.getValue().getId().getPath().contains("iron_xylophone"))
+                    type = 10;
+                else if (e.getValue().getId().getPath().contains("cow_bell"))
+                    type = 11;
+                else if (e.getValue().getId().getPath().contains("didgeridoo"))
+                    type = 12;
+                else if (e.getValue().getId().getPath().contains("bit"))
+                    type = 13;
+                else if (e.getValue().getId().getPath().contains("banjo"))
+                    type = 14;
+                else if (e.getValue().getId().getPath().contains("pling"))
+                    type = 15;
+                notes.add(Arrays.asList(ticks + 0, note, type));
+            }
+        }
 
-		prevSoundMap.clear();
-		prevSoundMap.putAll(soundMap);
-		if (!notes.isEmpty())
-			ticks++;
-	}
+        prevSoundMap.clear();
+        prevSoundMap.putAll(soundMap);
+        if (!notes.isEmpty())
+            ticks++;
+    }
 
 }
