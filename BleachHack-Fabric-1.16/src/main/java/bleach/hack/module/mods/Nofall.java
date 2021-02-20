d62b5b7d970f9c03e2a2697d61d821f287ce18ee
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-02-20 12:02:19 +0100

    ec.me Nofall

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/Nofall.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/Nofall.java
@@ -19,35 +19,52 @@ package bleach.hack.module.mods;
 
 import com.google.common.eventbus.Subscribe;
 
+import bleach.hack.event.events.EventSendPacket;
 import bleach.hack.event.events.EventTick;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
 import bleach.hack.setting.base.SettingMode;
+import bleach.hack.utils.FabricReflect;
 import net.minecraft.block.Blocks;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class Nofall extends Module {
+	
+	private boolean altBool = false;
 
 	public Nofall() {
 		super("Nofall", KEY_UNBOUND, Category.PLAYER, "Prevents you from taking fall damage.",
-				new SettingMode("Mode", "Simple", "Packet"));
+				new SettingMode("Mode", "Simple", "Packet", "ec.me").withDesc("Mode"));
 	}
 
 	@Subscribe
 	public void onTick(EventTick event) {
-		if (mc.player.fallDistance > 2f && getSetting(0).asMode().mode == 0) {
+		if (mc.player.fallDistance > 2.5f && getSetting(0).asMode().mode == 0) {
 			if (mc.player.isFallFlying())
 				return;
 			mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket(true));
 		}
 
-		if (mc.player.fallDistance > 2f && getSetting(0).asMode().mode == 1 &&
+		if (mc.player.fallDistance > 2.5f && getSetting(0).asMode().mode == 1 &&
 				mc.world.getBlockState(mc.player.getBlockPos().add(
 						0, -1.5 + (mc.player.getVelocity().y * 0.1), 0)).getBlock() != Blocks.AIR) {
 			mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket(false));
 			mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionOnly(
 					mc.player.getX(), mc.player.getY() - 420.69, mc.player.getZ(), true));
 			mc.player.fallDistance = 0;
+		}
+	}
+	
+	@Subscribe
+	public void onSendPacket(EventSendPacket event) {
+		if (event.getPacket() instanceof PlayerMoveC2SPacket && getSetting(0).asMode().mode == 2) {
+			if (mc.player.fallDistance > 2.5f && !altBool) {
+				mc.player.setOnGround(true);
+				FabricReflect.writeField(event.getPacket(), true, "field_12891", "onGround");
+				altBool = true;
+			} else {
+				altBool = false;
+			}
 		}
 	}
 
