25194a68b8f3d5f96c112e7cf142161bf3fd01be
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-02-26 15:54:51 +0100

    Fix tracers in 3rd person

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/Tracers.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/Tracers.java
@@ -64,8 +64,8 @@ public class Tracers extends Module {
 		for (Entity e : mc.world.getEntities()) {
 			Vec3d vec = e.getPos();
 
-			Vec3d vec2 = new Vec3d(0, 0, 75).rotateX(-(float) Math.toRadians(mc.cameraEntity.pitch))
-					.rotateY(-(float) Math.toRadians(mc.cameraEntity.yaw))
+			Vec3d vec2 = new Vec3d(0, 0, 75).rotateX(-(float) Math.toRadians(mc.gameRenderer.getCamera().getPitch()))
+					.rotateY(-(float) Math.toRadians(mc.gameRenderer.getCamera().getYaw()))
 					.add(mc.cameraEntity.getPos().add(0, mc.cameraEntity.getEyeHeight(mc.cameraEntity.getPose()), 0));
 
 			float[] col = null;
