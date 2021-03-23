8f6eada0607d3929662099d506e79658a2842c93
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-03-23 10:04:52 +0100

    Add $dupe book

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/command/commands/CmdDupe.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/command/commands/CmdDupe.java
@@ -17,7 +17,17 @@
  */
 package bleach.hack.command.commands;
 
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import bleach.hack.command.Command;
+import bleach.hack.util.BleachLogger;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.network.packet.c2s.play.BookUpdateC2SPacket;
 import net.minecraft.text.LiteralText;
 
 public class CmdDupe extends Command {
@@ -29,18 +39,40 @@ public class CmdDupe extends Command {
 
 	@Override
 	public String getDescription() {
-		return "Dupes an item on vanilla servers. (PATCHED IN 1.14.4+)";
+		return "Dupes items, (Vanilla mode patched on 1.14.4+)";
 	}
 
 	@Override
 	public String getSyntax() {
-		return "dupe";
+		return "dupe <vanilla/book>";
 	}
 
 	@Override
 	public void onCommand(String command, String[] args) throws Exception {
-		mc.player.dropSelectedItem(true);
-		mc.player.networkHandler.getConnection().disconnect(new LiteralText("Duping..."));
+		if (args[0].equalsIgnoreCase("vanilla")) {
+			mc.player.dropSelectedItem(true);
+			mc.player.networkHandler.getConnection().disconnect(new LiteralText("Duping..."));
+		} else if (args[0].equalsIgnoreCase("book")) {
+			ItemStack item = mc.player.inventory.getMainHandStack();
+
+			if (item.getItem() != Items.WRITABLE_BOOK) {
+				BleachLogger.errorMessage("Not holding a writable book!");
+				return;
+			}
+
+			IntStream chars = new Random().ints(0, 0x10ffff);
+			String text = chars.limit(25000 * 50).mapToObj(i -> String.valueOf((char) i)).collect(Collectors.joining());
+
+			ListTag textSplit = new ListTag();
+
+			for (int t = 0; t < 50; t++)
+				textSplit.add(StringTag.of(text.substring(t * 25000, (t + 1) * 25000)));
+
+			item.getOrCreateTag().put("pages", textSplit);
+			mc.player.networkHandler.sendPacket(new BookUpdateC2SPacket(item, false, mc.player.inventory.selectedSlot));
+		} else {
+			printSyntaxError("Invaild dupe method");
+		}
 	}
 
 }
