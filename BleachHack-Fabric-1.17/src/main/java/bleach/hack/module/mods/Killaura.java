54ccfe0bec2e2b35a11b028215f39117ef61616a
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-04-10 16:18:45 +0200

    Improved Killaura
    - Added Triggerbot setting
    - Added MultiAura setting
    - Added Sort setting
    - Fixed it spamming rotate packets while your weapon was on cooldown

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/Killaura.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/Killaura.java
@@ -17,7 +17,13 @@
  */
 package bleach.hack.module.mods;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.lwjgl.glfw.GLFW;
 
 import com.google.common.collect.Streams;
@@ -27,11 +33,13 @@ import bleach.hack.BleachHack;
 import bleach.hack.event.events.EventTick;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
+import bleach.hack.setting.base.SettingMode;
 import bleach.hack.setting.base.SettingSlider;
 import bleach.hack.setting.base.SettingToggle;
 import bleach.hack.setting.other.SettingRotate;
 import bleach.hack.util.world.EntityUtils;
 import bleach.hack.util.world.WorldUtils;
+import net.minecraft.client.render.debug.DebugRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.mob.Monster;
@@ -39,6 +47,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket.Mode;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 public class Killaura extends Module {
 
@@ -46,10 +56,14 @@ public class Killaura extends Module {
 
 	public Killaura() {
 		super("Killaura", GLFW.GLFW_KEY_K, Category.COMBAT, "Automatically attacks entities",
+				new SettingMode("Sort", "Angle", "Distance").withDesc("How to sort targets"),
 				new SettingToggle("Players", true).withDesc("Attack players"),
 				new SettingToggle("Mobs", true).withDesc("Attack mobs"),
 				new SettingToggle("Animals", false).withDesc("Attack animals"),
 				new SettingToggle("Armor Stands", false).withDesc("Attack armor stands"),
+				new SettingToggle("Triggerbot", false).withDesc("Only attacks the entity you are looking at"),
+				new SettingToggle("MultiAura", false).withDesc("Atacks multiple entities at once").withChildren(
+						new SettingSlider("Targets", 1, 20, 3, 0).withDesc("How many targets to attack at once")),
 				new SettingRotate(true),
 				new SettingToggle("Thru Walls", false).withDesc("Attack through walls"),
 				new SettingToggle("1.9 Delay", false).withDesc("Uses the 1.9+ delay between hits"),
@@ -64,31 +78,20 @@ public class Killaura extends Module {
 		}
 
 		delay++;
-		int reqDelay = (int) Math.round(20 / getSetting(8).asSlider().getValue());
+		int reqDelay = (int) Math.round(20 / getSetting(11).asSlider().getValue());
 
-		Optional<Entity> target = Streams.stream(mc.world.getEntities())
-				.filter(e -> !(e instanceof PlayerEntity && BleachHack.friendMang.has(e.getName().getString()))
-						&& mc.player.distanceTo(e) <= getSetting(7).asSlider().getValue()
-						&& e.isAlive() 
-						&& !e.getEntityName().equals(mc.getSession().getUsername())
-						&& e != mc.player.getVehicle()
-						&& (mc.player.canSee(e) || getSetting(5).asToggle().state))
-				.filter(e -> (e instanceof PlayerEntity && getSetting(0).asToggle().state)
-						|| (e instanceof Monster && getSetting(1).asToggle().state)
-						|| (EntityUtils.isAnimal(e) && getSetting(2).asToggle().state)
-						|| (e instanceof ArmorStandEntity && getSetting(3).asToggle().state))
-				.sorted((a, b) -> Float.compare(a.distanceTo(mc.player), b.distanceTo(mc.player)))
-				.findFirst();
+		boolean cooldownDone = getSetting(9).asToggle().state
+				? mc.player.getAttackCooldownProgress(mc.getTickDelta()) == 1.0f
+				: (delay > reqDelay || reqDelay == 0);
 
-		if (target.isPresent()) {
-			Entity e = target.get();
-			
-			if (getSetting(4).asRotate().state) {
-				WorldUtils.facePosAuto(e.getX(), e.getY() + e.getHeight() / 2, e.getZ(), getSetting(4).asRotate());
-			}
+		if (cooldownDone) {
+			for (Entity e: getEntities()) {
+				boolean shouldRotate = getSetting(7).asRotate().state && DebugRenderer.getTargetedEntity(mc.player, 7).orElse(null) != e;
 
-			if (((delay > reqDelay || reqDelay == 0) && !getSetting(6).asToggle().state) ||
-					(mc.player.getAttackCooldownProgress(mc.getTickDelta()) == 1.0f && getSetting(6).asToggle().state)) {
+				if (shouldRotate) {
+					WorldUtils.facePosAuto(e.getX(), e.getY() + e.getHeight() / 2, e.getZ(), getSetting(7).asRotate());
+				}
+
 				boolean wasSprinting = mc.player.isSprinting();
 
 				if (wasSprinting)
@@ -103,5 +106,57 @@ public class Killaura extends Module {
 				delay = 0;
 			}
 		}
+	}
+
+	private List<Entity> getEntities() {
+		Stream<Entity> targets;
+
+		if (getSetting(5).asToggle().state) {
+			Optional<Entity> entity = DebugRenderer.getTargetedEntity(mc.player, 7);
+
+			if (entity.isEmpty()) {
+				return Collections.emptyList();
+			}
+
+			targets = Stream.of(entity.get());
+		} else {
+			targets = Streams.stream(mc.world.getEntities());
+		}
+
+		Comparator<Entity> comparator;
+
+		if (getSetting(0).asMode().mode == 0) {
+			comparator = Comparator.comparing(e -> {
+				Vec3d center = e.getBoundingBox().getCenter();
+
+				double diffX = center.x - mc.player.getX();
+				double diffY = center.y - (mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()));
+				double diffZ = center.z - mc.player.getZ();
+
+				double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
+
+				float yaw = (float) Math.toDegrees(Math.atan2(diffZ, diffX)) - 90F;
+				float pitch = (float) -Math.toDegrees(Math.atan2(diffY, diffXZ));
+
+				return Math.abs(MathHelper.wrapDegrees(yaw - mc.player.yaw)) + Math.abs(MathHelper.wrapDegrees(pitch - mc.player.pitch));
+			});
+		} else {
+			comparator = Comparator.comparing(mc.player::distanceTo);
+		}
+
+		return targets
+				.filter(e -> !(e instanceof PlayerEntity && BleachHack.friendMang.has(e.getName().getString()))
+						&& e.isAlive() 
+						&& e != mc.player.getVehicle()
+						&& !e.getEntityName().equals(mc.getSession().getUsername())
+						&& mc.player.distanceTo(e) <= getSetting(10).asSlider().getValue()
+						&& (mc.player.canSee(e) || getSetting(8).asToggle().state))
+				.filter(e -> (e instanceof PlayerEntity && getSetting(1).asToggle().state)
+						|| (e instanceof Monster && getSetting(2).asToggle().state)
+						|| (EntityUtils.isAnimal(e) && getSetting(3).asToggle().state)
+						|| (e instanceof ArmorStandEntity && getSetting(4).asToggle().state))
+				.sorted(comparator)
+				.limit(getSetting(6).asToggle().state ? (long) getSetting(6).asToggle().getChild(0).asSlider().getValue() : 1L)
+				.collect(Collectors.toList());
 	}
 }
