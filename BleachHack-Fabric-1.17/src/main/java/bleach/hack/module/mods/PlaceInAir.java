f058b480dac519a95adcaf9f1ac899719236e409
lasnikprogram <lasnikprogram@gmail.com>
2021-04-14 17:12:09 +0200

    Improved PlaceInAir

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/PlaceInAir.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/PlaceInAir.java
@@ -5,6 +5,7 @@ import com.google.common.eventbus.Subscribe;
 import bleach.hack.event.events.EventSendPacket;
 import bleach.hack.event.events.EventTick;
 import bleach.hack.event.events.EventWorldRender;
+import bleach.hack.mixin.IMixinMinecraftClient;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
 import bleach.hack.setting.base.SettingColor;
@@ -12,60 +13,99 @@ import bleach.hack.setting.base.SettingMode;
 import bleach.hack.setting.base.SettingSlider;
 import bleach.hack.setting.base.SettingToggle;
 import bleach.hack.util.RenderUtils;
+import net.minecraft.block.BlockState;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+
+/**
+ * @author <a href="https://github.com/lasnikprogram">Lasnik</a>
+ */
 
 public class PlaceInAir extends Module {
 	private boolean pressed;
-	public static boolean shouldSendPacket;
 	
 	public PlaceInAir () {
-		super("PlaceInAir", KEY_UNBOUND, Category.WORLD, "Allows you to place blocks and entities in thin air",
-				new SettingToggle("Rendered", true).withDesc("Renders an overlay where it will place the block / entity").withChildren(
+		super("PlaceInAir", KEY_UNBOUND, Category.WORLD, "Allows you to place blocks in thin air",
+				new SettingToggle("Rendered", true).withDesc("Renders an overlay where it will place the block").withChildren(
 					new SettingMode("Render", "Box+Fill", "Box", "Fill").withDesc("The rendering method"),
 					new SettingSlider("Box", 0.1, 4, 2, 1).withDesc("The thickness of the box lines"),
 					new SettingSlider("Fill", 0, 1, 0.3, 2).withDesc("The opacity of the fill"),
-					new SettingColor("Color", 1f, 1f, 0f, false)));
+					new SettingColor("Color", 240f, 211f, 165f, false)),
+				new SettingMode("Mode", "Multi", "Single").withDesc("Whether to place a block once per click or multiple blocks if the button is held down"));
 	}
 	
 	@Subscribe
 	public void onTick (EventTick event) {
-		if (mc.options.keyUse.isPressed() && !pressed && mc.crosshairTarget instanceof BlockHitResult) {
-			shouldSendPacket = false;
-            PlayerInteractBlockC2SPacket packet = new PlayerInteractBlockC2SPacket(Hand.MAIN_HAND, (BlockHitResult) mc.crosshairTarget);
-            mc.getNetworkHandler().sendPacket(packet);
-            pressed = true;
+		boolean isKeyUsePressed = mc.options.keyUse.isPressed();
+		
+		if (!(mc.crosshairTarget instanceof BlockHitResult)) {
+			return;
 		}
+		
+		switch (getSetting(1).asMode().mode) {
+		
+		case 0:
+			if (((IMixinMinecraftClient) mc).getItemUseCooldown() == 4 && isKeyUsePressed) {
+				sendInteractionBlockC2SPacket();
+			}
+			break;
 			
-		else if (!mc.options.keyUse.isPressed()) {
-			pressed = false;
+		case 1:
+			if (!pressed && isKeyUsePressed) {
+				sendInteractionBlockC2SPacket();
+	            pressed = true;
+			} else if (!isKeyUsePressed) {
+				pressed = false;
+			}
+			break;
 		}
 	}
 	
 	@Subscribe
 	public void onWorldRender (EventWorldRender event) {
-		if (mc.crosshairTarget instanceof BlockHitResult && getSetting(0).asToggle().state) {
-			BlockPos pos = new BlockPos(mc.crosshairTarget.getPos());
-			int mode = getSetting(0).asToggle().getChild(0).asMode().mode;
-			if (mode == 0 || mode == 2) {
-				float fillAlpha = (float) getSetting(0).asToggle().getChild(2).asSlider().getValue();
-				float[] rgb = getSetting(0).asToggle().getChild(3).asColor().getRGBFloat();
-				RenderUtils.drawFill(pos, rgb[0], rgb[1], rgb[2], fillAlpha);
-			}
-			
-			if (mode == 0 || mode == 1) {
-				float outlineWidth = (float) getSetting(0).asToggle().getChild(1).asSlider().getValue();
-				float[] rgb = getSetting(0).asToggle().getChild(3).asColor().getRGBFloat();
-				RenderUtils.drawOutline(pos, rgb[0], rgb[1], rgb[2], 1f, outlineWidth);
+		if (!(mc.crosshairTarget instanceof BlockHitResult) || !getSetting(0).asToggle().state) {
+			return;
+		}
+		
+		BlockPos pos = new BlockPos(mc.crosshairTarget.getPos());
+		BlockState state = mc.world.getBlockState(pos);
+		VoxelShape voxelShape = state.getOutlineShape(mc.world, pos);
+		if (voxelShape.isEmpty()) {
+			voxelShape = VoxelShapes.cuboid(0, 0, 0, 1, 1, 1);
+		}
+		
+		int mode = getSetting(0).asToggle().getChild(0).asMode().mode;
+		float[] rgb = getSetting(0).asToggle().getChild(3).asColor().getRGBFloat();
+		
+		if (mode == 0 || mode == 1) {
+			float outlineWidth = (float) getSetting(0).asToggle().getChild(1).asSlider().getValue();
+			for (Box box: voxelShape.getBoundingBoxes()) {
+				RenderUtils.drawOutline(box.offset(pos), rgb[0], rgb[1], rgb[2], 1f, outlineWidth);
 			}
 		}
-	}	
+		if (mode == 0 || mode == 2) {
+			float fillAlpha = (float) getSetting(0).asToggle().getChild(2).asSlider().getValue();
+			for (Box box: voxelShape.getBoundingBoxes()) {
+				RenderUtils.drawFill(box.offset(pos), rgb[0], rgb[1], rgb[2], fillAlpha);
+			}
+		}
+	}
+	
 	
 	@Subscribe
-	public void onSendPacket(EventSendPacket event) {
-		if (event.getPacket() instanceof PlayerInteractBlockC2SPacket)
+	public void onSendPacket (EventSendPacket event) {
+		if (event.getPacket() instanceof PlayerInteractBlockC2SPacket) {
 			event.setCancelled(true);
+		}
+	}
+	
+	private void sendInteractionBlockC2SPacket() {
+		PlayerInteractBlockC2SPacket packet = new PlayerInteractBlockC2SPacket(Hand.MAIN_HAND, (BlockHitResult) mc.crosshairTarget);
+		mc.getNetworkHandler().sendPacket(packet);
 	}
 }
