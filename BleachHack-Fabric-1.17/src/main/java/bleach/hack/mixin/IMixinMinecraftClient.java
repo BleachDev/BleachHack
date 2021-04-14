9e3d27721056e5394c30024cd3807107369f25cc
lasnikprogram <lasnikprogram@gmail.com>
2021-04-14 17:22:36 +0200

    Added missing Mixin

---
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/mixin/IMixinMinecraftClient.java
@@ -0,0 +1,12 @@
+package bleach.hack.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.minecraft.client.MinecraftClient;
+
+@Mixin(MinecraftClient.class)
+public interface IMixinMinecraftClient {
+	@Accessor
+	public abstract int getItemUseCooldown();
+}
