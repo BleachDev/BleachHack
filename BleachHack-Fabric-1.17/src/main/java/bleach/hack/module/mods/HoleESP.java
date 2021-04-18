4388b2e570620ab05555d81045206a0af145f504
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-04-18 22:58:00 +0200

    Improved HoleESP
    - Made bottom render flat and moved it to a subsetting
    - Added RenderSides setting
    - Added HideWhenIn setting

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/HoleESP.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/HoleESP.java
@@ -1,6 +1,10 @@
 package bleach.hack.module.mods;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.util.Set;
 
 import com.google.common.eventbus.Subscribe;
@@ -15,14 +19,16 @@ import bleach.hack.setting.base.SettingSlider;
 import bleach.hack.setting.base.SettingToggle;
 import bleach.hack.util.render.RenderUtils;
 import bleach.hack.util.render.color.QuadColor;
+import bleach.hack.util.render.color.QuadColor.CardinalDirection;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 
 /**
  * @author <a href="https://github.com/lasnikprogram">Lasnik</a>
  */
-
 public class HoleESP extends Module {
 
 	private Set<BlockPos> bedrockHoles = new HashSet<>();
@@ -32,16 +38,22 @@ public class HoleESP extends Module {
 	public HoleESP() {
 		super("HoleESP", KEY_UNBOUND, Category.RENDER, "Highlights save and not so save holes. Used for CrystalPvP",
 				new SettingSlider("Radius", 1, 20, 10, 0).withDesc("Radius in which holes are getting searched"),
-				new SettingMode("Render", "Box+Fill", "Box", "Fill").withDesc("The rendering method"),
-				new SettingSlider("Box", 0.1, 4, 2, 1).withDesc("The thickness of the box lines"),
-				new SettingSlider("Fill", 0, 1, 0.3, 2).withDesc("The opacity of the fill"),
+				new SettingToggle("RenderBottom", true).withDesc("Render the bottom of this hole").withChildren(
+						new SettingMode("Render", "Box+Fill", "Box", "Fill").withDesc("The rendering method"),
+						new SettingSlider("Box", 0.1, 4, 2, 1).withDesc("The thickness of the box lines"),
+						new SettingSlider("Fill", 0, 1, 0.3, 2).withDesc("The opacity of the fill")),
+				new SettingToggle("RenderSides", true).withDesc("Render the sides of this hole").withChildren(
+						new SettingMode("Render", "GlowUp", "GlowDown", "Box+Fill", "Box", "Fill").withDesc("The rendering method"),
+						new SettingSlider("Box", 0.1, 4, 2, 1).withDesc("The thickness of the box lines"),
+						new SettingSlider("Fill", 0, 1, 0.3, 2).withDesc("The opacity of the fill/glow"),
+						new SettingSlider("Height", 0.1, 8, 1, 1).withDesc("The height to render the sides")),
 				new SettingToggle("Bedrock", true).withDesc("Shows holes with full bedrock").withChildren(
 						new SettingColor("Color", 0f, 1f, 0f, false).withDesc("Color for bedrock holes")),
 				new SettingToggle("Mixed", true).withDesc("Shows holes with a mix of obsidian and bedrock").withChildren(
 						new SettingColor("Mixed", 1f, 1f, 0f, false).withDesc("Color formixed holes")),
 				new SettingToggle("Obsidian", true).withDesc("Shows holes with a mix of obsidian and bedrock").withChildren(
 						new SettingColor("Obsidian", 1f, 0f, 0f, false).withDesc("Color for obsidian holes")),
-				new SettingMode("Location", "Under", "At", "Flat").withDesc("Where and how the box is rendered"));
+				new SettingToggle("HideWhenIn", true).withDesc("Hides the hole you're currently in to prevent blocking out screen"));
 	}
 
 	@Override
@@ -72,6 +84,14 @@ public class HoleESP extends Module {
 					|| !mc.world.getBlockState(pos.up(2)).getCollisionShape(mc.world, pos.up(2)).isEmpty()) {
 				continue;
 			}
+			
+			if (getSetting(6).asToggle().state
+					&& mc.player.getBoundingBox().getCenter().x > pos.getX() + 0.1
+					&& mc.player.getBoundingBox().getCenter().x < pos.getX() + 0.9
+					&& mc.player.getBoundingBox().getCenter().z > pos.getZ() + 0.1
+					&& mc.player.getBoundingBox().getCenter().z < pos.getZ() + 0.9) {
+				continue;
+			}
 
 			int bedrockCounter = 0;
 			int obsidianCounter = 0;
@@ -85,12 +105,12 @@ public class HoleESP extends Module {
 				}
 			}
 
-			if (bedrockCounter == 5 && getSetting(4).asToggle().state) {
+			if (bedrockCounter == 5 && getSetting(3).asToggle().state) {
 				bedrockHoles.add(pos.toImmutable());
-			} else if (obsidianCounter == 5 && getSetting(6).asToggle().state) {
+			} else if (obsidianCounter == 5 && getSetting(5).asToggle().state) {
 				obsidianHoles.add(pos.toImmutable());
-			} else if (bedrockCounter >= 1 && obsidianCounter >= 1 
-					&& bedrockCounter + obsidianCounter == 5 && getSetting(5).asToggle().state) {
+			} else if (bedrockCounter >= 1 && obsidianCounter >= 1
+					&& bedrockCounter + obsidianCounter == 5 && getSetting(4).asToggle().state) {
 				mixedHoles.add(pos.toImmutable());
 			}
 		}
@@ -98,43 +118,56 @@ public class HoleESP extends Module {
 
 	@Subscribe
 	public void onRender(EventWorldRender.Post event) {
-		int mode = getSetting(1).asMode().mode;
+		Map<BlockPos, float[]> blocks = new HashMap<>();
+		bedrockHoles.forEach(b -> blocks.put(b, getSetting(3).asToggle().getChild(0).asColor().getRGBFloat()));
+		mixedHoles.forEach(b -> blocks.put(b, getSetting(4).asToggle().getChild(0).asColor().getRGBFloat()));
+		obsidianHoles.forEach(b -> blocks.put(b, getSetting(5).asToggle().getChild(0).asColor().getRGBFloat()));
 
-		if (mode == 0 || mode == 2) {
-			float fillAlpha = (float) getSetting(3).asSlider().getValue();
+		if (getSetting(1).asToggle().state) {
+			int bottomMode = getSetting(1).asToggle().getChild(0).asMode().mode;
+			Direction[] excludeDirs = ArrayUtils.remove(Direction.values(), 0);
 
-			for (BlockPos pos : bedrockHoles) {
-				float[] rgb = getSetting(4).asToggle().getChild(0).asColor().getRGBFloat();
-				RenderUtils.drawBoxFill(boxAt(pos), QuadColor.single(rgb[0], rgb[1], rgb[2], fillAlpha));
+			if (bottomMode == 0 || bottomMode == 2) {
+				blocks.forEach((pos, color) -> {
+					RenderUtils.drawBoxFill(pos, QuadColor.single(color[0], color[1], color[2], (float) getSetting(1).asToggle().getChild(2).asSlider().getValue()), excludeDirs);
+				});
 			}
 
-			for (BlockPos pos : mixedHoles) {
-				float[] rgb = getSetting(5).asToggle().getChild(0).asColor().getRGBFloat();
-				RenderUtils.drawBoxFill(boxAt(pos), QuadColor.single(rgb[0], rgb[1], rgb[2], fillAlpha));
-			}
-
-			for (BlockPos pos : obsidianHoles) {
-				float[] rgb = getSetting(6).asToggle().getChild(0).asColor().getRGBFloat();
-				RenderUtils.drawBoxFill(boxAt(pos), QuadColor.single(rgb[0], rgb[1], rgb[2], fillAlpha));
+			if (bottomMode == 0 || bottomMode == 1) {
+				blocks.forEach((pos, color) -> {
+					RenderUtils.drawBoxOutline(pos, QuadColor.single(color[0], color[1], color[2], 1f), (float) getSetting(1).asToggle().getChild(1).asSlider().getValue(), excludeDirs);
+				});
 			}
 		}
 
-		if (mode == 0 || mode == 1) {
-			float outlineWidth = (float) getSetting(2).asSlider().getValue();
+		if (getSetting(2).asToggle().state) {
+			int sideMode = getSetting(2).asToggle().getChild(0).asMode().mode;
+			float height = (float) getSetting(2).asToggle().getChild(3).asSlider().getValue();
+			Direction[] excludeDirs = new Direction[] { Direction.UP, Direction.DOWN, };
 
-			for (BlockPos pos : bedrockHoles) {
-				float[] rgb = getSetting(4).asToggle().getChild(0).asColor().getRGBFloat();
-				RenderUtils.drawBoxOutline(boxAt(pos), QuadColor.single(rgb[0], rgb[1], rgb[2], 1f), outlineWidth);
-			}
+			if (sideMode == 0 || sideMode == 1) {
+				CardinalDirection gradientDir = sideMode == 0 ? CardinalDirection.NORTH : CardinalDirection.SOUTH;
 
-			for (BlockPos pos : mixedHoles) {
-				float[] rgb = getSetting(5).asToggle().getChild(0).asColor().getRGBFloat();
-				RenderUtils.drawBoxOutline(boxAt(pos), QuadColor.single(rgb[0], rgb[1], rgb[2], 1f), outlineWidth);
-			}
+				blocks.forEach((pos, color) -> {
+					RenderUtils.drawBoxFill(new Box(Vec3d.of(pos), Vec3d.of(pos).add(1, 0, 1)).stretch(0, height, 0),
+							QuadColor.gradient(
+									color[0], color[1], color[2], (float) getSetting(2).asToggle().getChild(2).asSlider().getValue(),
+									color[0], color[1], color[2], 0f, gradientDir), excludeDirs);
+				});
+			} else {
+				if (sideMode == 2 || sideMode == 4) {
+					blocks.forEach((pos, color) -> {
+						RenderUtils.drawBoxFill(new Box(Vec3d.of(pos), Vec3d.of(pos).add(1, 0, 1)).stretch(0, height, 0),
+								QuadColor.single(color[0], color[1], color[2], (float) getSetting(2).asToggle().getChild(2).asSlider().getValue()), excludeDirs);
+					});
+				}
 
-			for (BlockPos pos : obsidianHoles) {
-				float[] rgb = getSetting(6).asToggle().getChild(0).asColor().getRGBFloat();
-				RenderUtils.drawBoxOutline(boxAt(pos), QuadColor.single(rgb[0], rgb[1], rgb[2], 1f), outlineWidth);
+				if (sideMode == 2 || sideMode == 3) {
+					blocks.forEach((pos, color) -> {
+						RenderUtils.drawBoxOutline(new Box(Vec3d.of(pos), Vec3d.of(pos).add(1, 0, 1)).stretch(0, height, 0),
+								QuadColor.single(color[0], color[1], color[2], 1f), (float) getSetting(2).asToggle().getChild(1).asSlider().getValue(), excludeDirs);
+					});
+				}
 			}
 		}
 	}
@@ -143,15 +176,5 @@ public class HoleESP extends Module {
 		return new BlockPos[] {
 				pos.west(), pos.east(), pos.south(), pos.north(), pos.down()
 		};
-	}
-
-	private Box boxAt(BlockPos pos) {
-		int renderLocation = getSetting(7).asMode().mode;
-		Box box = new Box(renderLocation == 0 ? pos.down() : pos);
-
-		if (renderLocation == 2)
-			return box.expand(0, -0.5, 0).offset(0, -0.5, 0);
-
-		return box;
 	}
 }
