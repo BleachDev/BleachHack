b6067f9710a45de5c2ac344d0142c0ad09e2cccf
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-05-02 21:34:36 +0200

    Fix EntityControl spamming logs when joining a world

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/mixin/MixinLlamaPigStriderEntity.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/mixin/MixinLlamaPigStriderEntity.java
@@ -15,20 +15,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import bleach.hack.BleachHack;
 import bleach.hack.event.events.EventEntityControl;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.LlamaEntity;
 import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.entity.passive.StriderEntity;
+import net.minecraft.world.World;
 
 @Mixin({ LlamaEntity.class, PigEntity.class, StriderEntity.class })
-public abstract class MixinLlamaPigStriderEntity {
+public abstract class MixinLlamaPigStriderEntity extends AnimalEntity {
+
+	protected MixinLlamaPigStriderEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+		super(entityType, world);
+	}
 
 	@Inject(method = "canBeControlledByRider", at = @At("HEAD"), cancellable = true)
 	public void canBeControlledByRider(CallbackInfoReturnable<Boolean> info) {
-		EventEntityControl event = new EventEntityControl();
-		BleachHack.eventBus.post(event);
-		
-		if (event.canBeControlled() != null) {
-			info.setReturnValue(event.canBeControlled());
+		if (this.hasPassengers() && this.getPrimaryPassenger() == MinecraftClient.getInstance().player) {
+			EventEntityControl event = new EventEntityControl();
+			BleachHack.eventBus.post(event);
+
+			if (event.canBeControlled() != null) {
+				info.setReturnValue(event.canBeControlled());
+			}
 		}
 	}
 }
