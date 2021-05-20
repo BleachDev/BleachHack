6534a87189f2494ceb41f445259200c0f2e41d09
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-05-20 15:38:40 +0200

    Readd MountBypass and AutoIStackDupe because ?????????

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/mixin/MixinHandledScreen.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/mixin/MixinHandledScreen.java
@@ -8,32 +8,88 @@
  */
 package bleach.hack.mixin;
 
+import java.util.Arrays;
+
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import bleach.hack.BleachHack;
 import bleach.hack.event.events.EventDrawContainer;
+import bleach.hack.module.ModuleManager;
+import bleach.hack.module.mods.MountBypass;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.passive.AbstractDonkeyEntity;
+import net.minecraft.entity.passive.HorseBaseEntity;
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
 
 @Mixin(HandledScreen.class)
 public abstract class MixinHandledScreen extends Screen {
 
+	@Shadow protected int backgroundWidth;
+	@Shadow protected int backgroundHeight;
+
 	protected MixinHandledScreen(Text title) {
 		super(title);
+	}
+
+	@Inject(method = "init()V", at = @At("RETURN"))
+	protected void init(CallbackInfo info) {
+		if (client.player.getVehicle() instanceof HorseBaseEntity) {
+			int rightside = (width + backgroundWidth) / 2 + 2;
+			int topside = (height - backgroundHeight) / 2;
+
+			HorseBaseEntity entity = (HorseBaseEntity) client.player.getVehicle();
+
+			if (client.player.getVehicle() instanceof AbstractDonkeyEntity) {
+				addButton(new ButtonWidget(rightside, topside + 12, 50, 14, new LiteralText("AutoDupe"), button -> {
+					ModuleManager.getModule("AutoDonkeyDupe").setEnabled(true);
+				}));
+
+				addButton(new ButtonWidget(rightside, topside + 28, 50, 14, new LiteralText("Dupe"), button -> {
+					((MountBypass) ModuleManager.getModule("MountBypass")).dontCancel = true;
+
+					client.player.networkHandler.sendPacket(
+							PlayerInteractEntityC2SPacket.interactAt(
+									entity,
+									false,
+									Hand.MAIN_HAND,
+									entity.getPos().add(entity.getWidth() / 2, entity.getHeight() / 2, entity.getWidth() / 2)));
+
+					((MountBypass) ModuleManager.getModule("MountBypass")).dontCancel = false;
+				}));
+			}
+		}
 	}
 
 	@Inject(method = "render", at = @At("RETURN"))
 	public void render(MatrixStack matrix, int mouseX, int mouseY, float delta, CallbackInfo info) {
 		EventDrawContainer event = new EventDrawContainer(
 				(HandledScreen<?>) client.currentScreen, mouseX, mouseY, matrix); // hmm // hmm?
-
 		BleachHack.eventBus.post(event);
 		if (event.isCancelled())
 			info.cancel();
+
+		int rightside = (width + backgroundWidth) / 2 + 2;
+		int topside = (height - backgroundHeight) / 2;
+		if (client.player.getVehicle() instanceof AbstractDonkeyEntity) {
+			textRenderer.drawWithShadow(matrix, "IS Dupe: \u00a79[?]", rightside, topside + 2, -1);
+
+			if (mouseX >= rightside + textRenderer.getWidth("IS Dupe: ") && mouseX <= rightside + textRenderer.getWidth("IS Dupe: ") + 15
+					&& mouseY >= topside + 1 && mouseY <= topside + 11) {
+				renderTooltip(matrix, Arrays.asList(
+						new LiteralText("\u00a79IllegalStack dupe/Old endcrystal.me dupe"),
+						new LiteralText("\u00a79Only works on servers running IllegalStack <= 2.1.0")),
+						mouseX, mouseY);
+			}
+		}
 	}
 }
