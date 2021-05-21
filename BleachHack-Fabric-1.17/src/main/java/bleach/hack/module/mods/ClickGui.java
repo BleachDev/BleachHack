d33fd3ac70db60df7caeb2cd7a59f2dd53f16234
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-05-21 20:03:56 +0200

    Increased default clickgui length to 75

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/ClickGui.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/ClickGui.java
@@ -23,7 +23,7 @@ public class ClickGui extends Module {
 
 	public ClickGui() {
 		super("ClickGui", GLFW.GLFW_KEY_RIGHT_SHIFT, ModuleCategory.RENDER, "Draws the clickgui",
-				new SettingSlider("Length", 70, 85, 70, 0).withDesc("The length of each window"),
+				new SettingSlider("Length", 70, 85, 75, 0).withDesc("The length of each window"),
 				new SettingToggle("Search bar", false).withDesc("Shows a search bar"),
 				new SettingToggle("Help", true).withDesc("Shows the help text"));
 	}
