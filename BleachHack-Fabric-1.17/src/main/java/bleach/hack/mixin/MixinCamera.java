895539c754fec2f3dd390c2f272e7d3c92f30215
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-05-29 20:29:38 +0200

    Added BetterCamera (thanks CUPZYY)

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/mixin/MixinCamera.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/mixin/MixinCamera.java
@@ -9,10 +9,13 @@
 package bleach.hack.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import bleach.hack.module.Module;
 import bleach.hack.module.ModuleManager;
 import bleach.hack.module.mods.NoRender;
 import net.minecraft.client.render.Camera;
@@ -21,10 +24,33 @@ import net.minecraft.client.render.CameraSubmersionType;
 @Mixin(Camera.class)
 public class MixinCamera {
 
+	@Unique private boolean bypassCameraClip;
+
+	@Shadow private double clipToSpace(double desiredCameraDistance) { return 0; }
+
 	@Inject(method = "getSubmersionType", at = @At("HEAD"), cancellable = true)
 	private void getSubmergedFluidState(CallbackInfoReturnable<CameraSubmersionType> ci) {
 		if (((NoRender) ModuleManager.getModule("NoRender")).shouldRemoveOverlay(3)) {
 			ci.setReturnValue(CameraSubmersionType.NONE);
+		}
+	}
+
+	@Inject(method = "clipToSpace", at = @At("HEAD"), cancellable = true)
+	private void onClipToSpace(double desiredCameraDistance, CallbackInfoReturnable<Double> info) {
+		if (bypassCameraClip) {
+			bypassCameraClip = false;
+		} else {
+			Module betterCamera = ModuleManager.getModule("BetterCamera");
+
+			if (betterCamera.isEnabled()) {
+				if (betterCamera.getSetting(0).asToggle().state) {
+					info.setReturnValue(betterCamera.getSetting(1).asToggle().state
+							? betterCamera.getSetting(1).asToggle().getChild(0).asSlider().getValue() : desiredCameraDistance);
+				} else if (betterCamera.getSetting(1).asToggle().state) {
+					bypassCameraClip = true;
+					clipToSpace(desiredCameraDistance);
+				}
+			}
 		}
 	}
 }
