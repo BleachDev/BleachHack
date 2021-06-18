f990c1ac4cec74fbbaf0df5fa2c4b2d90ba32119
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-06-18 09:54:22 +0200

    Fixed $rbook not working with more than 100 pages

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/command/commands/CmdRbook.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/command/commands/CmdRbook.java
@@ -8,10 +8,7 @@
  */
 package bleach.hack.command.commands;
 
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import bleach.hack.command.Command;
@@ -44,13 +41,10 @@ public class CmdRbook extends Command {
 		int endChar = args.length >= 3 && NumberUtils.isCreatable(args[2]) ? NumberUtils.createNumber(args[2]).intValue() : 0x10FFFF;
 		int pageChars = args.length >= 4 && NumberUtils.isCreatable(args[3]) ? NumberUtils.createNumber(args[3]).intValue() : 210;
 
-		IntStream chars = new Random().ints(startChar, endChar + 1);
-		String text = chars.limit(pageChars * 100).mapToObj(i -> String.valueOf((char) i)).collect(Collectors.joining());
-
 		NbtList textSplit = new NbtList();
 
 		for (int t = 0; t < pages; t++)
-			textSplit.add(NbtString.of(text.substring(t * pageChars, (t + 1) * pageChars)));
+			textSplit.add(NbtString.of(RandomStringUtils.random(pageChars, startChar, endChar, false, false)));
 
 		item.getOrCreateTag().put("pages", textSplit);
 		mc.player.networkHandler.sendPacket(new BookUpdateC2SPacket(item, false, mc.player.getInventory().selectedSlot));
