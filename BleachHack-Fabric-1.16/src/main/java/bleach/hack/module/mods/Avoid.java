ef0a7b81fca75f74ff59e6462725ffe9ede8cf92
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-06-28 21:22:38 +0200

    Added Avoid
    actually being productive?!?!?!?!?!!?!

---
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/Avoid.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of the BleachHack distribution (https://github.com/BleachDrinker420/BleachHack/).
+ * Copyright (c) 2021 Bleach and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package bleach.hack.module.mods;
+
+import bleach.hack.eventbus.BleachSubscribe;
+import bleach.hack.event.events.EventBlockShape;
+import bleach.hack.event.events.EventClientMove;
+import bleach.hack.event.events.EventSendPacket;
+import bleach.hack.module.ModuleCategory;
+import bleach.hack.setting.base.SettingToggle;
+import net.minecraft.block.CactusBlock;
+import net.minecraft.block.FireBlock;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.network.packet.c2s.play.VehicleMoveC2SPacket;
+import net.minecraft.util.shape.VoxelShapes;
+import bleach.hack.module.Module;
+
+public class Avoid extends Module {
+
+	public Avoid() {
+		super("Avoid", KEY_UNBOUND, ModuleCategory.WORLD, "Adds collision boxes to certain blocks/areas",
+				new SettingToggle("Cactus", true).withDesc("Adds a bigger collision box to cactuses"),
+				new SettingToggle("Fire", true).withDesc("Adds a collision box to fire"),
+				new SettingToggle("Unloaded", true).withDesc("Adds walls to unloaded chunks"));
+	}
+
+	@BleachSubscribe
+	public void onBlockShape(EventBlockShape event) {
+		if ((getSetting(0).asToggle().state && event.getState().getBlock() instanceof CactusBlock)
+				|| (getSetting(1).asToggle().state && event.getState().getBlock() instanceof FireBlock)) {
+			event.setShape(VoxelShapes.fullCube());
+		}
+	}
+
+	@BleachSubscribe
+	public void onClientMove(EventClientMove event) {
+		if (getSetting(2).asToggle().state
+				&& !mc.world.getChunkManager().isChunkLoaded(
+						(int) (mc.player.getX() + event.getVec().x) >> 4, (int) (mc.player.getZ() + event.getVec().z) >> 4)) {
+			event.setCancelled(true);
+		}
+	}
+
+	@BleachSubscribe
+	public void onSendPacket(EventSendPacket event) {
+		if (getSetting(2).asToggle().state) {
+			if (event.getPacket() instanceof VehicleMoveC2SPacket) {
+				VehicleMoveC2SPacket packet = (VehicleMoveC2SPacket) event.getPacket();
+				if (!mc.world.getChunkManager().isChunkLoaded((int) packet.getX() >> 4, (int) packet.getZ() >> 4)) {
+					event.setCancelled(true);
+				}
+			} else if (event.getPacket() instanceof PlayerMoveC2SPacket) {
+				PlayerMoveC2SPacket packet = (PlayerMoveC2SPacket) event.getPacket();
+				if (!mc.world.getChunkManager().isChunkLoaded((int) packet.getX(mc.player.getX()) >> 4, (int) packet.getZ(mc.player.getZ()) >> 4)) {
+					event.setCancelled(true);
+				}
+			}
+		}
+	}
+}
---
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/event/events/EventBlockShape.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of the BleachHack distribution (https://github.com/BleachDrinker420/BleachHack/).
+ * Copyright (c) 2021 Bleach and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package bleach.hack.event.events;
+
+import bleach.hack.event.Event;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+
+public class EventBlockShape extends Event {
+
+	private BlockState state;
+	private BlockPos pos;
+	private VoxelShape shape;
+
+	public EventBlockShape(BlockState state, BlockPos pos, VoxelShape shape) {
+		this.state = state;
+		this.pos = pos;
+		this.setShape(shape);
+	}
+
+	public BlockState getState() {
+		return state;
+	}
+
+	public void setState(BlockState state) {
+		this.state = state;
+	}
+
+	public BlockPos getPos() {
+		return pos;
+	}
+
+	public void setPos(BlockPos pos) {
+		this.pos = pos;
+	}
+
+	public VoxelShape getShape() {
+		return shape;
+	}
+
+	public void setShape(VoxelShape shape) {
+		this.shape = shape;
+	}
+}
