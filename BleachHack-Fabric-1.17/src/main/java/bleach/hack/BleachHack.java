5144cff2f858b76c774ed0decbb266ba5b8674ea
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-07-10 12:02:35 +0200

    Bumped version to 1.1

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/BleachHack.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/BleachHack.java
@@ -27,8 +27,8 @@ public class BleachHack implements ModInitializer {
 
 	private static BleachHack instance = null;
 
-	public static final String VERSION = "1.0.1";
-	public static final int INTVERSION = 32;
+	public static final String VERSION = "1.1";
+	public static final int INTVERSION = 33;
 
 	public static BleachEventBus eventBus;
 
