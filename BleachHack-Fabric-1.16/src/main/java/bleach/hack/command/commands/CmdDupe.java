4a32bbd95b219d1724b01f678c13706bcd69d27b
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-07-10 11:59:31 +0200

    Removed $dupe old

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/command/commands/CmdDupe.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/command/commands/CmdDupe.java
@@ -10,60 +10,47 @@ package bleach.hack.command.commands;
 
 import bleach.hack.command.Command;
 import bleach.hack.command.CommandCategory;
-import bleach.hack.command.exception.CmdSyntaxException;
 import bleach.hack.util.BleachLogger;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtString;
 import net.minecraft.network.packet.c2s.play.BookUpdateC2SPacket;
-import net.minecraft.text.LiteralText;
 
 public class CmdDupe extends Command {
 
 	public CmdDupe() {
-		super("dupe", "Dupes items, (Old aka the 11/11 dupe is patched on 1.14.4+, Book mode works on all vanilla servers)", "dupe [old/book]", CommandCategory.MISC);
+		super("dupe", "Dupes items on VANILLA servers using the book dupe", "dupe", CommandCategory.MISC);
 	}
 
 	@Override
 	public void onCommand(String alias, String[] args) throws Exception {
-		if (args.length == 0) {
-			throw new CmdSyntaxException("Invaild dupe method");
+		if (mc.player.inventory.getMainHandStack().getItem() != Items.WRITABLE_BOOK) {
+			BleachLogger.errorMessage("Not holding a writable book!");
+			return;
 		}
 
-		if (args[0].equalsIgnoreCase("old")) {
-			mc.player.dropSelectedItem(true);
-			mc.player.networkHandler.getConnection().disconnect(new LiteralText("Duping..."));
-		} else if (args[0].equalsIgnoreCase("book")) {
-			if (mc.player.inventory.getMainHandStack().getItem() != Items.WRITABLE_BOOK) {
-				BleachLogger.errorMessage("Not holding a writable book!");
-				return;
-			}
+		NbtList listTag = new NbtList();
 
-			NbtList listTag = new NbtList();
+		StringBuilder builder1 = new StringBuilder();
+		for(int i = 0; i < 21845; i++)
+			builder1.append((char) 2077);
 
-			StringBuilder builder1 = new StringBuilder();
-			for(int i = 0; i < 21845; i++)
-				builder1.append((char) 2077);
+		listTag.add(0, NbtString.of(builder1.toString()));
 
-			listTag.add(0, NbtString.of(builder1.toString()));
+		StringBuilder builder2 = new StringBuilder();
+		for(int i = 0; i < 32; i++)
+			builder2.append("BleachHK");
 
-			StringBuilder builder2 = new StringBuilder();
-			for(int i = 0; i < 32; i++)
-				builder2.append("BleachHK");
+		String string2 = builder2.toString();
+		for(int i = 1; i < 40; i++)
+			listTag.add(i, NbtString.of(string2));
 
-			String string2 = builder2.toString();
-			for(int i = 1; i < 40; i++)
-				listTag.add(i, NbtString.of(string2));
+		ItemStack bookStack = new ItemStack(Items.WRITABLE_BOOK, 1);
+		bookStack.putSubTag("title", NbtString.of("If you can see this, it didn't work"));
+		bookStack.putSubTag("pages", listTag);
 
-			ItemStack bookStack = new ItemStack(Items.WRITABLE_BOOK, 1);
-			bookStack.putSubTag("title", NbtString.of("If you can see this, it didn't work"));
-			bookStack.putSubTag("pages", listTag);
-
-			mc.player.networkHandler.sendPacket(new BookUpdateC2SPacket(bookStack, true, mc.player.inventory.selectedSlot));
-		} else {
-			throw new CmdSyntaxException("Invaild dupe method");
-		}
+		mc.player.networkHandler.sendPacket(new BookUpdateC2SPacket(bookStack, true, mc.player.inventory.selectedSlot));
 	}
 
 }
