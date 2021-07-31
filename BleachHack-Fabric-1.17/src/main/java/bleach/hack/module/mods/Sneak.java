541586dca695bfcc7390a7e9366249e4b7f52587
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-07-31 10:11:05 +0200

    (somewhat) fixed Vp's sneak

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/Sneak.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/Sneak.java
@@ -1,5 +1,6 @@
 package bleach.hack.module.mods;
 
+import bleach.hack.event.events.EventSendPacket;
 import bleach.hack.event.events.EventTick;
 import bleach.hack.eventbus.BleachSubscribe;
 import bleach.hack.module.Module;
@@ -9,17 +10,48 @@ import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 
 public class Sneak extends Module {
 
+	private boolean packetSent;
+
 	public Sneak() {
 		super("Sneak", KEY_UNBOUND, ModuleCategory.MOVEMENT, "Makes you automatically sneak.",
 				new SettingMode("Mode", "Legit", "Packet").withDesc("Mode for sneaking (Only other players will see u sneaking with packet mode)."));
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+
+		packetSent = false;
+		mc.options.keySneak.setPressed(false);
+		mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.RELEASE_SHIFT_KEY));
+	}
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+
+		if (getSetting(0).asMode().mode == 1) {
+			mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.PRESS_SHIFT_KEY));
+			packetSent = true;
+		}
 	}
 
 	@BleachSubscribe
 	public void onTick(EventTick event) {
 		if (getSetting(0).asMode().mode == 0) {
 			mc.options.keySneak.setPressed(true);
-		} else {
+		} else if (getSetting(0).asMode().mode == 1 && !packetSent) {
 			mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.PRESS_SHIFT_KEY));
+			packetSent = true;
+		}
+	}
+
+	@BleachSubscribe
+	public void onSendPacket(EventSendPacket event) {
+		if (event.getPacket() instanceof ClientCommandC2SPacket) {
+			ClientCommandC2SPacket p = (ClientCommandC2SPacket) event.getPacket();
+			if (p.getMode() == ClientCommandC2SPacket.Mode.RELEASE_SHIFT_KEY)
+				event.setCancelled(true);
 		}
 	}
 }
