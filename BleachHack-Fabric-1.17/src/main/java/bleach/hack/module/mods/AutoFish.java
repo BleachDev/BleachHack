0ed6354a0f6946898ba80f6b19c383c38253bb37
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-08-08 23:05:47 +0200

    Made autofish work with both hands and made it switch to the best rod on enable

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/AutoFish.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/AutoFish.java
@@ -5,29 +5,67 @@ package bleach.hack.module.mods;
 
 import bleach.hack.eventbus.BleachSubscribe;
 
+import java.util.Comparator;
+
 import bleach.hack.event.events.EventSoundPlay;
 import bleach.hack.module.ModuleCategory;
-import net.minecraft.item.FishingRodItem;
+import bleach.hack.util.BleachLogger;
+import bleach.hack.util.InventoryUtils;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
 import bleach.hack.module.Module;
 
 public class AutoFish extends Module {
 
 	public AutoFish() {
-		super("AutoFish", KEY_UNBOUND, ModuleCategory.PLAYER, "Automatically fish.");
+		super("AutoFish", KEY_UNBOUND, ModuleCategory.PLAYER, "Automatically fishes for you.");
 	}
 
 	@Override
-	public void onDisable() {
-		super.onDisable();
+	public void onEnable() {
+		super.onEnable();
+
+		int slot = getBestRodSlot();
+		if (slot == -1) {
+			BleachLogger.error("No fishing rods in your inventory!");
+			setEnabled(false);
+			return;
+		}
+
+		InventoryUtils.selectSlot(slot);
 	}
 
 	@BleachSubscribe
 	public void onSoundPlay(EventSoundPlay.Normal event) {
-		if (event.instance.getId().getPath().equals("entity.fishing_bobber.splash") && mc.player.getMainHandStack().getItem() instanceof FishingRodItem) {
-			//reel back
-			mc.interactionManager.interactItem(mc.player, mc.world, mc.player.preferredHand);
-			//throw again
-			mc.interactionManager.interactItem(mc.player, mc.world, mc.player.preferredHand);
+		if (event.instance.getId().getPath().equals("entity.fishing_bobber.splash")) {
+			Hand hand = mc.player.getMainHandStack().getItem() == Items.FISHING_ROD ? Hand.MAIN_HAND
+					: mc.player.getOffHandStack().getItem() == Items.FISHING_ROD ? Hand.OFF_HAND
+							: null;
+
+			if (hand != null) {
+				//reel back
+				mc.interactionManager.interactItem(mc.player, mc.world, hand);
+				//throw again
+				mc.interactionManager.interactItem(mc.player, mc.world, hand);
+			}
 		}
+	}
+
+	private int getBestRodSlot() {
+		int slot = InventoryUtils.getSlot(true, false, Comparator.comparingInt(i -> {
+			ItemStack is = mc.player.getInventory().getStack(i);
+			if (is.getItem() != Items.FISHING_ROD)
+				return -1;
+
+			return EnchantmentHelper.get(is).values().stream().mapToInt(Integer::intValue).sum();
+		}));
+
+		if (mc.player.getInventory().getStack(slot).getItem() == Items.FISHING_ROD) {
+			return slot;
+		}
+
+		return -1;
 	}
 }
