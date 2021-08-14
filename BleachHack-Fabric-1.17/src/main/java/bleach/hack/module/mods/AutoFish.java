987b337934b60ce61adc64a22d30e0d1da87f332
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2021-08-14 14:46:43 +0200

    1

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/AutoFish.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/AutoFish.java
@@ -55,7 +55,7 @@ public class AutoFish extends Module {
 		SoundInstance si = event.instance;
 		if (si.getId().getPath().equals("entity.fishing_bobber.splash")
 				&& mc.player.fishHook != null
-				&& mc.player.fishHook.getPos().distanceTo(new Vec3d(si.getX(), si.getY(), si.getZ())) <= 1) {
+				&& mc.player.fishHook.getPos().distanceTo(new Vec3d(si.getX(), si.getY(), si.getZ())) <= 2) {
 			Hand hand = getHandWithRod();
 
 			if (hand != null) {
