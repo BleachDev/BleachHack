59653eb1f2e30d66609fe8d8a26c6896e98fa04a
BleachDrinker420 <36450388+BleachDrinker420@users.noreply.github.com>
2021-10-30 18:26:26 +0200

    Made password textboxes less buggy

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/gui/window/widget/WindowPassTextFieldWidget.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/gui/window/widget/WindowPassTextFieldWidget.java
@@ -1,30 +1,50 @@
 package bleach.hack.gui.window.widget;
 
-import java.lang.reflect.Field;
+import java.util.function.Function;
 
-import bleach.hack.util.BleachLogger;
 import bleach.hack.util.FabricReflect;
+import net.minecraft.client.font.FontStorage;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Matrix4f;
 
 public class WindowPassTextFieldWidget extends WindowTextFieldWidget {
 
-	private static final Field TEXT_FIELD = FabricReflect.getField(TextFieldWidget.class, "field_2092", "text");
-
 	public WindowPassTextFieldWidget(int x, int y, int width, int height, String text) {
-		super(x, y, width, height, text);
+		super(x, y, width, height);
+		this.textField = new TextFieldWidget(createTextRenderer(), x, y, width, height, LiteralText.EMPTY);
+		this.textField.setText(text);
+		this.textField.setMaxLength(32767);
 	}
 
-	@Override
-	public void render(MatrixStack matrices, int windowX, int windowY, int mouseX, int mouseY) {
-		String realText = textField.getText();
+	@SuppressWarnings("unchecked")
+	private TextRenderer createTextRenderer() {
+		Function<Identifier, FontStorage> thing = (Function<Identifier, FontStorage>) FabricReflect.getFieldValue(mc.textRenderer, "field_1997", "fontStorageAccessor");
+		return new TextRenderer(thing) {
+			@Override
+			public int draw(String text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider vertexConsumers, boolean seeThrough, int backgroundColor, int light, boolean rightToLeft) {
+				return super.draw(hide(text), x, y, color, shadow, matrix, vertexConsumers, seeThrough, backgroundColor, light, rightToLeft);
+			}
 
-		try {
-			TEXT_FIELD.set(textField, new String(new char[realText.length()]).replace("\0", "\u2022"));
-			super.render(matrices, windowX, windowY, mouseX, mouseY);
-			TEXT_FIELD.set(textField, realText);
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			BleachLogger.logger.error("Error reflecting TextFieldWidget text field");
-		}
+			@Override
+			public String trimToWidth(String text, int maxWidth) {
+				return super.trimToWidth(hide(text), maxWidth);
+			}
+
+			@Override
+			public int getWidth(String text) {
+				return super.getWidth(hide(text));
+			}
+
+			private String hide(String text) {
+				if (text != textField.getText()) // literal equal becasue dabruh
+					return text;
+
+				return new String(new char[textField.getText().length()]).replace('\0', '\u2022');
+			}
+		};
 	}
 }
--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/gui/window/widget/WindowTextFieldWidget.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/gui/window/widget/WindowTextFieldWidget.java
@@ -8,12 +8,16 @@ import net.minecraft.text.LiteralText;
 public class WindowTextFieldWidget extends WindowWidget {
 
 	public TextFieldWidget textField;
-	
+
 	public WindowTextFieldWidget(int x, int y, int width, int height, String text) {
 		super(x, y, x + width + 1, y + height + 1);
-		this.textField = new TextFieldWidget(MinecraftClient.getInstance().textRenderer, x, y, width, height, LiteralText.EMPTY);
+		this.textField = new TextFieldWidget(mc.textRenderer, x, y, width, height, LiteralText.EMPTY);
 		this.textField.setText(text);
 		this.textField.setMaxLength(32767);
+	}
+	
+	protected WindowTextFieldWidget(int x, int y, int width, int height) {
+		super(x, y, x + width + 1, y + height + 1);
 	}
 
 	@Override
@@ -31,25 +35,25 @@ public class WindowTextFieldWidget extends WindowWidget {
 
 		textField.mouseClicked(mouseX, mouseY, button);
 	}
-	
+
 	@Override
 	public void tick() {
 		super.tick();
-		
+
 		textField.tick();
 	}
 
 	@Override
 	public void charTyped(char chr, int modifiers) {
 		super.charTyped(chr, modifiers);
-		
+
 		textField.charTyped(chr, modifiers);
 	}
 
 	@Override
 	public void keyPressed(int keyCode, int scanCode, int modifiers) {
 		super.keyPressed(keyCode, scanCode, modifiers);
-		
+
 		textField.keyPressed(keyCode, scanCode, modifiers);
 	}
 }
