1679d253139639102d37a8695652bb7beec3dc58
BleachDrinker420 <36450388+BleachDrinker420@users.noreply.github.com>
2021-11-29 21:48:01 +0100

    bump version

--- src/main/java/org/bleachhack/BleachHack.java
+++ src/main/java/org/bleachhack/BleachHack.java
@@ -38,8 +38,8 @@ public class BleachHack implements ModInitializer {
 
 	private static BleachHack instance = null;
 
-	public static final String VERSION = "1.2.2";
-	public static final int INTVERSION = 36;
+	public static final String VERSION = "1.2.3";
+	public static final int INTVERSION = 37;
 	public static Watermark watermark;
 
 	public static BleachEventBus eventBus;
