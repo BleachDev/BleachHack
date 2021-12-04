b620592346348b9fea2a86464cf7a91727662dcb
BleachDrinker420 <36450388+BleachDrinker420@users.noreply.github.com>
2021-12-04 12:19:37 +0100

    chumksiz 1.18!!!!!

--- src/main/java/org/bleachhack/util/world/ClientChunkSerializer.java
+++ src/main/java/org/bleachhack/util/world/ClientChunkSerializer.java
@@ -9,6 +9,7 @@
 package org.bleachhack.util.world;
 
 import java.util.Map;
+
 import com.mojang.serialization.Codec;
 
 import net.minecraft.SharedConstants;
@@ -17,7 +18,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtLongArray;
 import net.minecraft.nbt.NbtOps;
@@ -44,11 +44,11 @@ import net.minecraft.world.gen.carver.CarvingMask;
 import net.minecraft.world.gen.chunk.BlendingData;
 
 public class ClientChunkSerializer {
-	
+
 	private static final Codec<PalettedContainer<BlockState>> CODEC = PalettedContainer.createCodec(Block.STATE_IDS, BlockState.CODEC, PalettedContainer.PaletteProvider.BLOCK_STATE, Blocks.AIR.getDefaultState());
 	private static final String UPGRADE_DATA_KEY = "UpgradeData";
-    private static final String BLOCK_TICKS = "block_ticks";
-    private static final String FLUID_TICKS = "fluid_ticks";
+	private static final String BLOCK_TICKS = "block_ticks";
+	private static final String FLUID_TICKS = "fluid_ticks";
 
 	public static NbtCompound serialize(ClientWorld world, Chunk chunk) {
 		UpgradeData upgradeData;
@@ -62,20 +62,20 @@ public class ClientChunkSerializer {
 		nbtCompound.putLong("LastUpdate", world.getTime());
 		nbtCompound.putLong("InhabitedTime", chunk.getInhabitedTime());
 		nbtCompound.putString("Status", chunk.getStatus().getId());
-		
+
 		BlendingData blendingData = chunk.getBlendingData();
 		if (blendingData != null) {
-			BlendingData.CODEC.encodeStart(NbtOps.INSTANCE, blendingData).result().ifPresent(nbtElement -> nbtCompound.put("blending_data", (NbtElement)nbtElement));
+			BlendingData.CODEC.encodeStart(NbtOps.INSTANCE, blendingData).result().ifPresent(nbtElement -> nbtCompound.put("blending_data", nbtElement));
 		}
-		
+
 		if ((belowZeroRetrogen = chunk.getBelowZeroRetrogen()) != null) {
-			BelowZeroRetrogen.CODEC.encodeStart(NbtOps.INSTANCE, belowZeroRetrogen).result().ifPresent(nbtElement -> nbtCompound.put("below_zero_retrogen", (NbtElement)nbtElement));
+			BelowZeroRetrogen.CODEC.encodeStart(NbtOps.INSTANCE, belowZeroRetrogen).result().ifPresent(nbtElement -> nbtCompound.put("below_zero_retrogen", nbtElement));
 		}
-		
+
 		if (!(upgradeData = chunk.getUpgradeData()).isDone()) {
 			nbtCompound.put(UPGRADE_DATA_KEY, upgradeData.toNbt());
 		}
-		
+
 		ChunkSection[] chunkSections = chunk.getSectionArray();
 		NbtList nbtList = new NbtList();
 		LightingProvider lightingProvider = world.getChunkManager().getLightingProvider();
@@ -90,38 +90,41 @@ public class ClientChunkSerializer {
 			if (!bl2 && chunkNibbleArray == null && chunkNibbleArray2 == null) continue;
 			NbtCompound nbtCompound2 = new NbtCompound();
 			if (bl2) {
-				ChunkSection chunkSection = chunkSections[j2];
-				nbtCompound2.put("block_states", CODEC.encodeStart(NbtOps.INSTANCE, chunkSection.getBlockStateContainer()).get().left().orElse(new NbtCompound()));
-				nbtCompound2.put("biomes", codec.encodeStart(NbtOps.INSTANCE, chunkSection.getBiomeContainer()).get().left().orElse(new NbtCompound()));
+				try {
+					ChunkSection chunkSection = chunkSections[j2];
+					nbtCompound2.put("block_states", CODEC.encodeStart(NbtOps.INSTANCE, chunkSection.getBlockStateContainer()).get().left().orElse(new NbtCompound()));
+					nbtCompound2.put("biomes", codec.encodeStart(NbtOps.INSTANCE, chunkSection.getBiomeContainer()).get().left().orElse(new NbtCompound()));
+				} catch (Exception ignored) {
+				}
 			}
-			
+
 			if (chunkNibbleArray != null && !chunkNibbleArray.isUninitialized()) {
 				nbtCompound2.putByteArray("BlockLight", chunkNibbleArray.asByteArray());
 			}
-			
+
 			if (chunkNibbleArray2 != null && !chunkNibbleArray2.isUninitialized()) {
 				nbtCompound2.putByteArray("SkyLight", chunkNibbleArray2.asByteArray());
 			}
-			
+
 			if (nbtCompound2.isEmpty())
 				continue;
 
-			nbtCompound2.putByte("Y", (byte)i);
+			nbtCompound2.putByte("Y", (byte) i);
 			nbtList.add(nbtCompound2);
 		}
-		
+
 		nbtCompound.put("sections", nbtList);
 		if (bl) {
 			nbtCompound.putBoolean("isLightOn", true);
 		}
-		
+
 		NbtList i = new NbtList();
 		for (BlockPos bl2 : chunk.getBlockEntityPositions()) {
 			NbtCompound nbtCompound2 = chunk.getPackedBlockEntityNbt(bl2);
 			if (nbtCompound2 == null) continue;
 			i.add(nbtCompound2);
 		}
-		
+
 		nbtCompound.put("block_entities", i);
 		if (chunk.getStatus().getChunkType() == ChunkStatus.ChunkType.PROTOCHUNK) {
 			NbtList bl2 = new NbtList();
@@ -133,13 +136,13 @@ public class ClientChunkSerializer {
 				CarvingMask carvingMask = ((ProtoChunk) chunk).getCarvingMask(carver);
 				if (carvingMask == null)
 					continue;
-			
+
 				nbtCompound3.putLongArray(carver.toString(), carvingMask.getMask());
 			}
-			
+
 			nbtCompound.put("CarvingMasks", nbtCompound3);
 		}
-		
+
 		serializeTicks(world, nbtCompound, chunk.getTickSchedulers());
 		nbtCompound.put("PostProcessing", ChunkSerializer.toNbt(chunk.getPostProcessingLists()));
 		NbtCompound dabruh = new NbtCompound();
@@ -147,18 +150,18 @@ public class ClientChunkSerializer {
 			if (!chunk.getStatus().getHeightmapTypes().contains(entry.getKey())) continue;
 			dabruh.put(entry.getKey().getName(), new NbtLongArray(entry.getValue().asLongArray()));
 		}
-		
+
 		nbtCompound.put("Heightmaps", dabruh);
 		return nbtCompound;
 	}
 
 	private static Codec<PalettedContainer<Biome>> createCodec(Registry<Biome> biomeRegistry) {
-        return PalettedContainer.createCodec(biomeRegistry, biomeRegistry.getCodec(), PalettedContainer.PaletteProvider.BIOME, biomeRegistry.getOrThrow(BiomeKeys.PLAINS));
-    }
-	
+		return PalettedContainer.createCodec(biomeRegistry, biomeRegistry.getCodec(), PalettedContainer.PaletteProvider.BIOME, biomeRegistry.getOrThrow(BiomeKeys.PLAINS));
+	}
+
 	private static void serializeTicks(ClientWorld world, NbtCompound nbt, Chunk.TickSchedulers tickSchedulers) {
-        long l = world.getLevelProperties().getTime();
-        nbt.put(BLOCK_TICKS, tickSchedulers.blocks().toNbt(l, block -> Registry.BLOCK.getId(block).toString()));
-        nbt.put(FLUID_TICKS, tickSchedulers.fluids().toNbt(l, fluid -> Registry.FLUID.getId(fluid).toString()));
-    }
+		long l = world.getLevelProperties().getTime();
+		nbt.put(BLOCK_TICKS, tickSchedulers.blocks().toNbt(l, block -> Registry.BLOCK.getId(block).toString()));
+		nbt.put(FLUID_TICKS, tickSchedulers.fluids().toNbt(l, fluid -> Registry.FLUID.getId(fluid).toString()));
+	}
 }
