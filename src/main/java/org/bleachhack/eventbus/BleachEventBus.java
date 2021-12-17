5bb60f70e5d2d65a7363af88f4468272c3451817
BleachDrinker420 <36450388+BleachDrinker420@users.noreply.github.com>
2021-12-17 11:44:06 +0100

    Fixed AutoFish if multiple players were fishing next to each other

--- src/main/java/org/bleachhack/eventbus/BleachEventBus.java
+++ src/main/java/org/bleachhack/eventbus/BleachEventBus.java
@@ -7,26 +7,26 @@ import org.bleachhack.eventbus.registry.BleachSubscriberRegistry;
 public class BleachEventBus {
 
 	private final Logger logger;
-	private final BleachSubscriberRegistry subscriberRegistry;
+	private final BleachSubscriberRegistry registry;
 
 	public BleachEventBus(BleachSubscriberRegistry registry, Logger logger) {
-		this.subscriberRegistry = registry;
+		this.registry = registry;
 		this.logger = logger;
 	}
 
 	public boolean subscribe(Object object) {
-		return subscriberRegistry.subscribe(object);
+		return registry.subscribe(object);
 	}
 
 	public boolean unsubscribe(Object object) {
-		return subscriberRegistry.unsubscribe(object);
+		return registry.unsubscribe(object);
 	}
 
 	public void post(Event event) {
-		subscriberRegistry.post(event, logger);
+		registry.post(event, logger);
 	}
 
 	public long getEventsPosted() {
-		return subscriberRegistry.getEventsPosted();
+		return registry.getEventsPosted();
 	}
 }
--- src/main/java/org/bleachhack/module/mods/AutoFish.java
+++ src/main/java/org/bleachhack/module/mods/AutoFish.java
@@ -5,7 +5,6 @@ package org.bleachhack.module.mods;
 
 import java.util.Comparator;
 
-import org.bleachhack.event.events.EventSoundPlay;
 import org.bleachhack.event.events.EventTick;
 import org.bleachhack.eventbus.BleachSubscribe;
 import org.bleachhack.module.Module;
@@ -13,16 +12,16 @@ import org.bleachhack.module.ModuleCategory;
 import org.bleachhack.module.setting.base.SettingMode;
 import org.bleachhack.util.InventoryUtils;
 
-import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.Vec3d;
 
 public class AutoFish extends Module {
 
-	private boolean threwRod = false;
+	private boolean threwRod;
+	private boolean reeledFish;
 
 	public AutoFish() {
 		super("AutoFish", KEY_UNBOUND, ModuleCategory.PLAYER, "Automatically fishes for you.",
@@ -32,36 +31,37 @@ public class AutoFish extends Module {
 	@Override
 	public void onDisable(boolean inWorld) {
 		threwRod = false;
+		reeledFish = false;
 
 		super.onDisable(inWorld);
 	}
 
 	@BleachSubscribe
 	public void onTick(EventTick event) {
-		if (mc.player.fishHook != null)
+		if (mc.player.fishHook != null) {
 			threwRod = false;
+
+			boolean caughtFish = mc.player.fishHook.getDataTracker().get(FishingBobberEntity.CAUGHT_FISH);
+			if (!reeledFish && caughtFish) {
+				Hand hand = getHandWithRod();
+				if (hand != null) {
+					// reel back
+					mc.interactionManager.interactItem(mc.player, mc.world, hand);
+					reeledFish = true;
+					return;
+				}
+			} else if (!caughtFish) {
+				reeledFish = false;
+			}
+		}
 
 		if (!threwRod && mc.player.fishHook == null && getSetting(0).asMode().mode != 2) {
 			Hand newHand = getSetting(0).asMode().mode == 1 ? InventoryUtils.selectSlot(getBestRodSlot()) : getHandWithRod();
 			if (newHand != null) {
-				//throw again
+				// throw again
 				mc.interactionManager.interactItem(mc.player, mc.world, newHand);
 				threwRod = true;
-			}
-		}
-	}
-
-	@BleachSubscribe
-	public void onSoundPlay(EventSoundPlay.Normal event) {
-		SoundInstance si = event.getInstance();
-		if (si.getId().getPath().equals("entity.fishing_bobber.splash")
-				&& mc.player.fishHook != null
-				&& mc.player.fishHook.getPos().distanceTo(new Vec3d(si.getX(), si.getY(), si.getZ())) <= 2) {
-			Hand hand = getHandWithRod();
-
-			if (hand != null) {
-				//reel back
-				mc.interactionManager.interactItem(mc.player, mc.world, hand);
+				reeledFish = false;
 			}
 		}
 	}
