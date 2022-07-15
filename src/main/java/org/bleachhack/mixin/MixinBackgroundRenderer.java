7c998c483c7b15f63f4b8123a201b49d19ef84d9
DarkReaper231 <blackcreepercz012345@gmail.com>
2022-07-15 17:04:42 +0200

    Updates

--- src/main/java/org/bleachhack/mixin/MixinBackgroundRenderer.java
+++ src/main/java/org/bleachhack/mixin/MixinBackgroundRenderer.java
@@ -8,29 +8,31 @@
  */
 package org.bleachhack.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.Camera;
 import org.bleachhack.module.ModuleManager;
 import org.bleachhack.module.mods.NoRender;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffects;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BackgroundRenderer.class)
 public class MixinBackgroundRenderer {
 
-	@Redirect(method = {
-			"render(Lnet/minecraft/client/render/Camera;FLnet/minecraft/client/world/ClientWorld;IF)V",
-			"applyFog(Lnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/BackgroundRenderer$FogType;FZ)V"},
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;hasStatusEffect(Lnet/minecraft/entity/effect/StatusEffect;)Z"))
-	private static boolean hasStatusEffect(LivingEntity entity, StatusEffect effect) {
-		if (effect == StatusEffects.BLINDNESS && ModuleManager.getModule(NoRender.class).isOverlayToggled(0)) {
-			return false;
+	@Inject(method = "applyFog", at = @At("TAIL"))
+	private static void onApplyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
+		if (ModuleManager.getModule(NoRender.class).isOverlayToggled(0)) {
+			if (fogType == BackgroundRenderer.FogType.FOG_TERRAIN) {
+				RenderSystem.setShaderFogStart(viewDistance * 4);
+				RenderSystem.setShaderFogEnd(viewDistance * 4.25f);
+			}
 		}
-
-		return entity.hasStatusEffect(effect);
 	}
 }
