551e61d407923d8eac9ee3ef5d9dd867bd538f74
DarkReaper231 <blackcreepercz012345@gmail.com>
2022-09-15 21:43:37 +0200

    fixes

--- src/main/java/org/bleachhack/module/mods/IamBot.java
+++ src/main/java/org/bleachhack/module/mods/IamBot.java
@@ -2,11 +2,9 @@ package org.bleachhack.module.mods;
 
 import org.bleachhack.module.Module;
 import org.bleachhack.module.ModuleCategory;
-import org.bleachhack.setting.module.SettingMode;
 
 public class IamBot extends Module {
     public IamBot() {
-        super("IamBot", KEY_UNBOUND, ModuleCategory.MOVEMENT, "Makes you move like a bot.",
-                new SettingMode("Mode", "100x", "1000x").withDesc("Switches between two movement methods"));
+        super("Derp", KEY_UNBOUND, ModuleCategory.MOVEMENT, "work in progress");
     }
 }
