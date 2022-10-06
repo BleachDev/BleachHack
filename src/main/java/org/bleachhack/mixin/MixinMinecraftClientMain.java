64a37153fea2f2d647096807debeee2b56e9c9f5
DarkReaper231 <blackcreepercz012345@gmail.com>
2022-10-06 10:58:50 +0200

    update

--- src/main/java/org/bleachhack/mixin/MixinMinecraftClientMain.java
+++ src/main/java/org/bleachhack/mixin/MixinMinecraftClientMain.java
@@ -2,14 +2,13 @@ package org.bleachhack.mixin;
 
 import net.minecraft.client.main.Main;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(Main.class)
 public class MixinMinecraftClientMain {
-    @Inject(method = "main([Ljava/lang/String;Z)V", at = @At("HEAD"))
-    private static void whyHeadless(String[] args, boolean bl, CallbackInfo ci) {
-        System.setProperty("java.awt.headless", "false");
+    @ModifyConstant(method = "<clinit>", constant = @Constant(stringValue = "true"))
+    private static String whyHeadless(String constant) {
+        return "false";
     }
 }
