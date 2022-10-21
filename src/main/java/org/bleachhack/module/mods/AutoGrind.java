ce7d478478aa59bd76fc4b2deaceafca8cf81954
Austin Burk <apburk@gmail.com>
2022-10-20 21:09:32 -0400

    Enchanted book bug fixed, but slot logic is a bit iffy depending on timing

--- src/main/java/org/bleachhack/module/mods/AutoGrind.java
+++ src/main/java/org/bleachhack/module/mods/AutoGrind.java
@@ -8,6 +8,7 @@ import org.bleachhack.setting.module.SettingItemList;
 import org.bleachhack.util.BleachLogger;
 
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.GrindstoneScreenHandler;
@@ -37,25 +38,59 @@ public class AutoGrind extends Module {
 
 		GrindstoneScreenHandler handler = (GrindstoneScreenHandler) mc.player.currentScreenHandler;
 
+		// if there's already an item in the grindstone, don't do anything
+		if (!(handler.getSlot(0).getStack().isEmpty() && handler.getSlot(1).getStack().isEmpty()))
+			return;
+
 		for (int slot = 3; slot <= 38; slot++) {
 			ItemStack stack = handler.getSlot(slot).getStack();
-			if (getSetting(0).asList(Item.class).contains(stack.getItem())) {
+			// if this is one of the items on our list
+			if (shouldGrind(stack)) {
 				// if item has grindable enchants
-				int enchants = EnchantmentHelper.get(stack).values().stream().mapToInt(Integer::intValue).sum();
-				int curses = (EnchantmentHelper.hasVanishingCurse(stack) ? 1 : 0)
-						+ (EnchantmentHelper.hasBindingCurse(stack) ? 1 : 0);
-
-				if (enchants - curses > 0) {
+				if (canGrind(stack)) {
 					// shift-click item into gridstone slot
 					mc.interactionManager.clickSlot(handler.syncId, slot, 0, SlotActionType.QUICK_MOVE, mc.player);
-					// pick up from grindstone output slot (2)
-					mc.interactionManager.clickSlot(handler.syncId, 2, 0, SlotActionType.PICKUP, mc.player);
-					// click the original slot to put the de-enchanted item back
-					mc.interactionManager.clickSlot(handler.syncId, slot, 0, SlotActionType.PICKUP, mc.player);
+					// grind
+					doGrind(handler, slot);
 					// wait til next tick before grinding another one
 					return;
 				}
+				// continue to next item
 			}
 		}
+	}
+
+	private boolean shouldGrind(ItemStack stack) {
+		return getSetting(0).asList(Item.class).contains(stack.getItem());
+	}
+
+	private boolean canGrind(ItemStack stack) {
+		int enchants = getEnchantCount(stack);
+		int curses = getCurseCount(stack);
+		return (enchants - curses > 0);
+	}
+
+	private void doGrind(GrindstoneScreenHandler handler, int destinationSlot) {
+		// pick up from grindstone output slot (2)
+		mc.interactionManager.clickSlot(handler.syncId, 2, 0, SlotActionType.PICKUP, mc.player);
+		// click the original slot to put the de-enchanted item back
+		mc.interactionManager.clickSlot(handler.syncId, destinationSlot, 0, SlotActionType.PICKUP, mc.player);
+	}
+
+	private int getEnchantCount(ItemStack stack) {
+		// get the number of entries from EnchantmentHelper.get(stack)
+		return EnchantmentHelper.get(stack).size();
+	}
+
+	private int getCurseCount(ItemStack stack) {
+		// needed as EnchantmentHelper's curse checks are bugged for Enchanted Books
+		int curses = 0;
+		if (EnchantmentHelper.get(stack).containsKey(Enchantments.BINDING_CURSE)) {
+			curses += EnchantmentHelper.get(stack).get(Enchantments.BINDING_CURSE);
+		}
+		if (EnchantmentHelper.get(stack).containsKey(Enchantments.VANISHING_CURSE)) {
+			curses += EnchantmentHelper.get(stack).get(Enchantments.VANISHING_CURSE);
+		}
+		return curses;
 	}
 }
