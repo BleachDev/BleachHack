e0e06061d025b41b5bed5edcd8560e7d051f2adf
BleachDrinker420 <36450388+BleachDrinker420@users.noreply.github.com>
2023-03-30 15:50:39 +0200

    Fix Fabric api compatibility

--- src/main/java/org/bleachhack/mixin/MixinShader.java
+++ src/main/java/org/bleachhack/mixin/MixinShader.java
@@ -10,9 +10,13 @@ package org.bleachhack.mixin;
 
 import net.minecraft.client.gl.ShaderProgram;
 import net.minecraft.client.gl.ShaderStage;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.resource.ResourceFactory;
@@ -21,29 +25,30 @@ import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 // Tweaks to the shader class to make it compatible with OpenResourceManager
-@Mixin(ShaderProgram.class)
+@Mixin(value = ShaderProgram.class, priority = 1100)
 public class MixinShader {
 
-	// Redirect because optifine is tarded
-	@Redirect(method = "<init>", at = @At(value = "NEW", target = "(Ljava/lang/String;)Lnet/minecraft/util/Identifier;"))
-	private static Identifier init_identifier(String string, ResourceFactory factory, String name, VertexFormat format) {
-		return new Identifier(replaceIdentifier(string, name));
+	@Shadow @Final private String name;
+
+	@ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;<init>(Ljava/lang/String;)V"), allow = 1)
+	private String modifyProgramId(String id) {
+		return replaceIdentifier(id, name);
 	}
 
-	@ModifyArgs(method = "loadShader", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;<init>(Ljava/lang/String;)V"))
-	private static void loadShader_identifier(Args args, ResourceFactory factory, ShaderStage.Type type, String name) {
-		args.set(0, replaceIdentifier(args.get(0), name));
+	@ModifyVariable(method = "loadShader", at = @At("STORE"), ordinal = 1)
+	private static String modifyStageId(String id, ResourceFactory factory, ShaderStage.Type type, String name) {
+		return replaceIdentifier(id, name);
 	}
 	
-	private static String replaceIdentifier(String string, String name) {
+	private static String replaceIdentifier(String id, String name) {
 		String[] split = name.split(":");
-		if (split.length > 1) {
+		if (split.length > 1 && id.indexOf('/') < id.indexOf(':')) {
 			if ("__url__".equals(split[0]))
 				return name;
 
-			return split[0] + ":" + string.replace(name, split[1]);
+			return split[0] + ":" + id.replace(name, split[1]);
 		}
 
-		return string;
+		return id;
 	}
 }
